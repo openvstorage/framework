e217605e5eac6d8ed40b590d575a75c5617856a1
khenderi <kenneth@ketronic.be>
2013-10-09 10:33:49 +0200

    Few extra tests and updated the template docs

--- ovsdal/tests/test_performance.py
+++ ovsdal/tests/test_performance.py
@@ -5,6 +5,7 @@ from ovsdal.storage.memcached import MemcacheStore
 from ovsdal.hybrids.disk import Disk
 from ovsdal.hybrids.machine import Machine
 from ovsdal.storedobject import StoredObject
+from ovsdal.datalist import DataList
 
 
 class LotsOfObjects(TestCase):
@@ -30,7 +31,7 @@ class LotsOfObjects(TestCase):
                 disk.save()
             seconds_passed = (time.time() - start)
             itemspersec = ((i + 1) * 100.0) / seconds_passed
-            print '* machine %d/100 (%s/s)' % (i, str(itemspersec))
+            print '* machine %d/100 (creating %s disks per second)' % (i, str(itemspersec))
         print 'loading done'
 
         print 'start queries'
@@ -39,8 +40,8 @@ class LotsOfObjects(TestCase):
             machine = Machine(mguids[i])
             self.assertEqual(len(machine.disks), 100, 'Not all disks were retreived')
             seconds_passed = (time.time() - start)
-            itemspersec = ((i + 1) * 100.0) / seconds_passed
-            print '* machine %d/100 (%s/s)' % (i, str(itemspersec))
+            itemspersec = ((i + 1) * 10000.0) / seconds_passed
+            print '* machine %d/100 (filtering %s disks per second)' % (i, str(itemspersec))
         print 'completed'
 
         print 'start cached queries'
@@ -48,8 +49,27 @@ class LotsOfObjects(TestCase):
         for i in xrange(0, 100):
             machine = Machine(mguids[i])
             self.assertEqual(len(machine.disks), 100, 'Not all disks were retreived')
-            seconds_passed = (time.time() - start)
-            itemspersec = ((i + 1) * 100.0) / seconds_passed
-            print '* machine %d/100 (%s/s)' % (i, str(itemspersec))
-        print 'completed'
+        seconds_passed = (time.time() - start)
+        print 'completed in %d seconds' % seconds_passed
 
+        print 'start full query on disk property'
+        start = time.time()
+        amount = DataList(key   = 'size_between_4k_7k',
+                          query = {'object': Disk,
+                                   'data': DataList.select.COUNT,
+                                   'query': {'type': DataList.where_operator.AND,
+                                             'items': [('size', DataList.operator.GT, 4000),
+                                                       ('size', DataList.operator.LT, 7000)]}}).data
+        self.assertEqual(amount, 2900, 'Correct number of disks should be found. Found: %s' % str(amount))
+        seconds_passed = (time.time() - start)
+        print 'completed in %d seconds (filtering %d disks per second)' % (seconds_passed, (10000.0 / seconds_passed))
+
+        print 'cleaning up'
+        start = time.time()
+        for i in xrange(0, 100):
+            machine = Machine(mguids[i])
+            for disk in machine.disks:
+                disk.delete()
+            machine.delete()
+        seconds_passed = (time.time() - start)
+        print 'completed in %d seconds' % seconds_passed
