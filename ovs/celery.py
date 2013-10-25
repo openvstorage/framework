538571ff50552463ed8df58599ec2f56a5106735
khenderi <kenneth@ketronic.be>
2013-10-25 11:35:19 +0200

    Added support for celery beat + Added a snapshot policy + Cleaned/modified the celery config stuff

--- ovs/celery.py
+++ ovs/celery.py
@@ -1,12 +1,10 @@
 from __future__ import absolute_import
 
-import time
-import logging
 import sys
 sys.path.append('/opt/openvStorage')
 
 from celery import Celery
-from ovs import celeryconfig
+from celery.schedules import crontab
 from ovs.logging.logHandler import LogHandler
 
 celery = Celery('ovs',
@@ -15,8 +13,28 @@ celery = Celery('ovs',
                          'ovs.lib.vmachine',
                          'ovs.lib.user',
                          'ovs.lib.messaging',
+                         'ovs.lib.scheduledtask',
                          'ovs.hypervisor.hypervisors.vmware'])
-celery.config_from_object(celeryconfig)
+
+celery.conf.CELERY_RESULT_BACKEND = "cache"
+celery.conf.CELERY_CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
+celery.conf.BROKER_URL = 'amqp://guest:guest@127.0.0.1:5672//'
+celery.conf.CELERYBEAT_SCHEDULE = {
+    # Snapshot policy
+    # > Executes every weekday between 2 and 22 hour, every 15 minutes
+    'take-snapshots': {
+        'task': 'ovs.scheduled.snapshotall',
+        'schedule': crontab(minute='0', hour='2-22', day_of_week='mon,tue,wed,thu,fri'),
+        'args': [],
+    },
+    # Dummy
+    # > Executes every minute
+    #'dummy': {
+    #    'task': 'ovs.scheduled.dummy',
+    #    'schedule': crontab(minute='*'),
+    #    'args': []
+    #}
+}
 
 loghandler = LogHandler('celery.log')
 
--- ovs/celeryconfig.py
+++
@@ -1,3 +0,0 @@
-CELERY_RESULT_BACKEND = "cache"
-CELERY_CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
-BROKER_URL = 'amqp://guest:guest@127.0.0.1:5672//'
