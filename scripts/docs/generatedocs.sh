195ec4e12a186dd602bdba95bca7b0b62da06b2a
khenderi <kenneth@ketronic.be>
2013-11-21 09:12:09 +0100

    Hide outputs for missing dependencies in doc generation

--- scripts/docs/generatedocs.sh
+++ scripts/docs/generatedocs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-epydoc --graph umlclasstree ovs -o $1 -v --exclude=test --no-sourcecode --no-private
+epydoc --graph umlclasstree ovs -o $1 -v --exclude=test --no-sourcecode --no-private > /dev/null
