822729c5f185eaf156068072117b962be58fdb22
khenderi <kenneth@ketronic.be>
2013-11-25 11:16:50 +0100

    Small tweak to doc generation

--- scripts/docs/generatedocs.sh
+++ scripts/docs/generatedocs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm -rf $1/*
 mkdir $1/lib
-epydoc --graph umlclasstree ovs.lib -o $1/lib -v --exclude=test --no-sourcecode --no-private --parse-only > /dev/null
+epydoc --graph umlclasstree ovs.lib -o $1/lib -v --no-sourcecode --no-private --parse-only > /dev/null
 mkdir $1/dal
-epydoc --graph umlclasstree ovs.dal -o $1/dal -v --exclude=test --no-sourcecode --no-private > /dev/null
+epydoc --graph umlclasstree ovs.dal -o $1/dal -v --no-sourcecode --no-private > /dev/null
 mkdir $1/extensions
-epydoc --graph umlclasstree ovs.extensions -o $1/extensions -v --exclude=test --no-sourcecode --no-private > /dev/null
+epydoc --graph umlclasstree ovs.extensions -o $1/extensions -v --no-sourcecode --no-private > /dev/null
 mkdir $1/hypervisor
-epydoc --graph umlclasstree ovs.hypervisor -o $1/hypervisor -v --exclude=test --no-sourcecode --no-private --parse-only > /dev/null
+epydoc --graph umlclasstree ovs.hypervisor -o $1/hypervisor -v --no-sourcecode --no-private --parse-only > /dev/null
