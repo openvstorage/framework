e4ed6b0b2f45e642861558c98ce6bee848f49cd4
Kenneth Henderick <kenneth@ketronic.be>
2013-11-28 14:37:56 +0100

    Change generate docs script to prevent it from wiping / when no parameters are given

--- scripts/docs/generatedocs.sh
+++ scripts/docs/generatedocs.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-rm -rf $1/*
-mkdir $1/lib
-epydoc --graph umlclasstree ovs.lib -o $1/lib -v --no-sourcecode --no-private --parse-only > /dev/null
-mkdir $1/dal
-epydoc --graph umlclasstree ovs.dal -o $1/dal -v --no-sourcecode --no-private > /dev/null
-mkdir $1/extensions
-epydoc --graph umlclasstree ovs.extensions -o $1/extensions -v --no-sourcecode --no-private > /dev/null
-mkdir $1/hypervisor
-epydoc --graph umlclasstree ovs.hypervisor -o $1/hypervisor -v --no-sourcecode --no-private --parse-only > /dev/null
+DIR=${1%/}
+if [ "$(ls -A $DIR/)" ]; then
+     echo "Directory $DIR/ must be empty"
+     exit 1
+fi
+mkdir $DIR/lib
+epydoc --graph umlclasstree ovs.lib -o $DIR/lib -v --no-sourcecode --no-private --parse-only > /dev/null
+mkdir $DIR/dal
+epydoc --graph umlclasstree ovs.dal -o $DIR/dal -v --no-sourcecode --no-private > /dev/null
+mkdir $DIR/extensions
+epydoc --graph umlclasstree ovs.extensions -o $DIR/extensions -v --no-sourcecode --no-private > /dev/null
+mkdir $DIR/hypervisor
+epydoc --graph umlclasstree ovs.hypervisor -o $DIR/hypervisor -v --no-sourcecode --no-private --parse-only > /dev/null
