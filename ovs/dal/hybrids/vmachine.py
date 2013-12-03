6b2b8a79d32298c0231e060d59ca521df4e22aec
pploegae <philippe.ploegaert@openvstorage.com>
2013-12-03 13:14:29 +0100

    OVS-71 - added vmachine snapshots

--- ovs/dal/hybrids/vmachine.py
+++ ovs/dal/hybrids/vmachine.py
@@ -4,7 +4,12 @@ VMachine module
 """
 from ovs.dal.dataobject import DataObject
 from ovs.dal.hybrids.pmachine import PMachine
+from ovs.extensions.storageserver.volumestoragerouter import VolumeStorageRouterClient
+from collections import OrderedDict
 
+_vsrClient = VolumeStorageRouterClient().load()
+
+import pickle
 
 class VMachine(DataObject):
     """
@@ -12,12 +17,12 @@ class VMachine(DataObject):
     or a Virtual Machine running the Open vStorage software.
     """
     # pylint: disable=line-too-long
-    _blueprint = {'name':         (None,  str,  'Name of the vMachine.'),
-                  'description':  (None,  str,  'Description of the vMachine.'),
-                  'hypervisorid': (None,  str,  'The Identifier of the vMachine on the Hypervisor.'),
-                  'is_vtemplate': (True,  bool, 'Indicates whether this vMachine is a vTemplate.'),
+    _blueprint = {'name':         (None, str, 'Name of the vMachine.'),
+                  'description':  (None, str, 'Description of the vMachine.'),
+                  'hypervisorid': (None, str, 'The Identifier of the vMachine on the Hypervisor.'),
+                  'is_vtemplate': (True, bool, 'Indicates whether this vMachine is a vTemplate.'),
                   'is_internal':  (False, bool, 'Indicates whether this vMachine is a Management VM for the Open vStorage Framework.'),
-                  'hvtype':       (None,  ['HYPERV', 'VMWARE', 'XEN'], 'Hypervisor type serving the vMachine.')}
+                  'hvtype':       (None, ['HYPERV', 'VMWARE', 'XEN'], 'Hypervisor type serving the vMachine.')}
     _relations = {'pmachine': (PMachine, 'vmachines')}
     _expiry = {'snapshots':     (60, list),
                'status':        (30, str),
@@ -31,27 +36,32 @@ class VMachine(DataObject):
         Fetches a list of Snapshots for the vMachine.
         @return: list
         """
-        _ = self
-        import time
-        import uuid
-        first = time.time() - (60 * 60 * 24 * 3)
-        second = time.time() - (60 * 60 * 24 * 2)
-        third = time.time() - (60 * 60 * 24)
-        return [{'timestamp': first,
-                 'label': 'My first snapshot',
-                 'is_consistent': True,
-                 'snapshots': {str(uuid.uuid4()): uuid.uuid4(),
-                               str(uuid.uuid4()): uuid.uuid4()}},
-                {'timestamp': second,
-                 'label': None,
-                 'is_consistent': False,
-                 'snapshots': {str(uuid.uuid4()): uuid.uuid4(),
-                               str(uuid.uuid4()): uuid.uuid4()}},
-                {'timestamp': third,
-                 'label': 'My Third snapshot',
-                 'is_consistent': True,
-                 'snapshots': {str(uuid.uuid4()): uuid.uuid4(),
-                               str(uuid.uuid4()): uuid.uuid4()}}]
+        snapshots = list()
+        _tmp_snapshots = OrderedDict()
+        for disk in self.vdisks:
+            for guid in disk.snapshots:
+                snapshot = _vsrClient.info_snapshot(str(disk.volumeid), guid)
+                metadata = pickle.loads(snapshot.metadata)
+                timestamp = metadata['timestamp']
+                if _tmp_snapshots.has_key(timestamp):
+                    _tmp_snapshots[timestamp]['snapshots'][disk.guid] = guid
+                else:
+                    snapshot_default = {'label' : metadata['label'],
+                                        'is_consistent' : metadata['is_consistent'],
+                                        'snapshots' : dict()
+                                        }
+                    snapshot_default['snapshots'][disk.guid] = guid
+                    _tmp_snapshots[timestamp] = snapshot_default
+        OrderedDict(sorted(_tmp_snapshots.items(), key=lambda k: k[0]))
+        for k, v in _tmp_snapshots.iteritems():
+            entry = dict()
+            entry['timestamp'] = k
+            entry['label'] = v['label']
+            entry['is_consistent'] = v['is_consistent']
+            entry['snapshots'] = v['snapshots']
+            snapshots.append(entry)
+
+        return snapshots
 
     def _status(self):
         """
@@ -82,7 +92,7 @@ class VMachine(DataObject):
 
     def _failover_mode(self):
         """
-        Gets the agregated failover mode
+        Gets the aggregated failover mode
         """
         status = None
         for disk in self.vdisks:
