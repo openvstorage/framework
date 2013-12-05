1fd2245393f9947e6ca6db59e663a705cc9839e5
Kenneth Henderick <kenneth@ketronic.be>
2013-12-05 09:54:58 +0100

    Added support for DAL statistics and added them to #full/statistics

--- webapps/frontend/app/viewmodels/containers/memcached.js
+++ webapps/frontend/app/viewmodels/containers/memcached.js
@@ -24,6 +24,23 @@ define([
         self.uptime       = ko.observable(0);
         self.raw          = ko.observable('');
 
+        self.ovsDRhit   = ko.observable(0);
+        self.ovsDRtot   = ko.observable(0);
+        self.ovsDRrat   = ko.observable(0);
+        self.ovsDRspeed = ko.deltaObservable(generic.formatShort);
+        self.ovsOLhit   = ko.observable(0);
+        self.ovsOLtot   = ko.observable(0);
+        self.ovsOLrat   = ko.observable(0);
+        self.ovsOLspeed = ko.deltaObservable(generic.formatShort);
+        self.ovsDLhit   = ko.observable(0);
+        self.ovsDLtot   = ko.observable(0);
+        self.ovsDLrat   = ko.observable(0);
+        self.ovsDLspeed = ko.deltaObservable(generic.formatShort);
+        self.ovsRLhit   = ko.observable(0);
+        self.ovsRLtot   = ko.observable(0);
+        self.ovsRLrat   = ko.observable(0);
+        self.ovsRLspeed = ko.deltaObservable(generic.formatShort);
+
         // Functions
         self.refresh = function() {
             generic.xhrAbort(self.refreshHandle);
@@ -34,10 +51,27 @@ define([
                 self.totalItems(data.total_items);
                 self.getHits(data.get_hits);
                 self.cmdGet(data.cmd_get);
-                self.hitRate(data.cmd_get === 0 ? 0 : Math.round(data.get_hits / data.cmd_get * 1000) / 10);
+                self.hitRate(data.cmd_get === 0 ? 100 : generic.round(data.get_hits / data.cmd_get * 100, 2));
                 self.bytesRead(generic.formatBytes(data.bytes_read));
                 self.bytesWritten(generic.formatBytes(data.bytes_written));
                 self.uptime(data.uptime);
+
+                self.ovsDRhit(data.ovs_dal.descriptor_hit);
+                self.ovsDRtot(data.ovs_dal.descriptor_hit + data.ovs_dal.descriptor_miss);
+                self.ovsDRrat(self.ovsDRtot() === 0 ? 100 : generic.round(self.ovsDRhit() / self.ovsDRtot() * 100, 2));
+                self.ovsDRspeed(self.ovsDRtot());
+                self.ovsOLhit(data.ovs_dal.object_load_hit);
+                self.ovsOLtot(data.ovs_dal.object_load_hit + data.ovs_dal.object_load_miss);
+                self.ovsOLrat(self.ovsOLtot() === 0 ? 100 : generic.round(self.ovsOLhit() / self.ovsOLtot() * 100, 2));
+                self.ovsOLspeed(self.ovsOLtot());
+                self.ovsDLhit(data.ovs_dal.datalist_hit);
+                self.ovsDLtot(data.ovs_dal.datalist_hit + data.ovs_dal.datalist_miss);
+                self.ovsDLrat(self.ovsDLtot() === 0 ? 100 : generic.round(self.ovsDLhit() / self.ovsDLtot() * 100, 2));
+                self.ovsDLspeed(self.ovsDLtot());
+                self.ovsRLhit(data.ovs_dal.relations_hit);
+                self.ovsRLtot(data.ovs_dal.relations_hit + data.ovs_dal.relations_miss);
+                self.ovsRLrat(self.ovsRLtot() === 0 ? 100 : generic.round(self.ovsRLhit() / self.ovsRLtot() * 100, 2));
+                self.ovsRLspeed(self.ovsRLtot());
 
                 var rawString = '', attribute;
                 for (attribute in data) {
