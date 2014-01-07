3495c05c9b324d95cac4f972f6b3eba2597b1059
khenderi <kenneth@ketronic.be>
2014-01-07 17:15:48 +0100

    OVS-249 - Reduce memory footprint by adding double locking to the hypervisor factory singleton so only once SDK per process can be loaded.

--- config/django/django_gunicorn_ovs.cfg.py
+++ config/django/django_gunicorn_ovs.cfg.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 bind = '127.0.0.1:8000'
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count() + 1
 backlog = 2048
 worker_class = 'gevent'
 worker_connections = 1000
--- ovs/extensions/hypervisor/factory.py
+++ ovs/extensions/hypervisor/factory.py
@@ -5,6 +5,7 @@ Hypervisor factory module
 from hypervisors.vmware import VMware
 from hypervisors.hyperv import HyperV
 from hypervisors.xen import Xen
+from ovs.extensions.generic.filemutex import FileMutex
 
 
 class Factory(object):
@@ -23,15 +24,21 @@ class Factory(object):
         ip       = node.ip
         username = node.username
         password = node.password
-        key = '{0}_{1}_{2}'.format(ip, username, password)
+        key = '{0}_{1}'.format(ip, username)
         if key not in Factory.hypervisors:
-            if hvtype == 'HYPERV':
-                hypervisor = HyperV(ip, username, password)
-            elif hvtype == 'VMWARE':
-                hypervisor = VMware(ip, username, password)
-            elif hvtype == 'XEN':
-                hypervisor = Xen(ip, username, password)
-            else:
-                raise Exception('Invalid hypervisor')
-            Factory.hypervisors[key] = hypervisor
+            mutex = FileMutex('hypervisor_{0}'.format(key))
+            try:
+                mutex.acquire()
+                if key not in Factory.hypervisors:
+                    if hvtype == 'HYPERV':
+                        hypervisor = HyperV(ip, username, password)
+                    elif hvtype == 'VMWARE':
+                        hypervisor = VMware(ip, username, password)
+                    elif hvtype == 'XEN':
+                        hypervisor = Xen(ip, username, password)
+                    else:
+                        raise Exception('Invalid hypervisor')
+                    Factory.hypervisors[key] = hypervisor
+            finally:
+                mutex.release()
         return Factory.hypervisors[key]
