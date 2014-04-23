a77e11ed91c831a4ddbca42cbc750f8ae1704ed0
Kenneth Henderick <kenneth@ketronic.be>
2014-04-23 14:17:09 +0200

    OVS-804 - Add export workaround for 14.04 blocked install.

--- config/env/OpenvStorage.sh
+++ config/env/OpenvStorage.sh
@@ -1,6 +1,7 @@
 # OpenvStorage profile: executed by Bourne-compatible login shells.
 
 export PYTHONPATH="${PYTHONPATH}:/opt/OpenvStorage:/opt/OpenvStorage/webapps"
+export DISPLAY=:0
 
 ovs() {
   source /opt/OpenvStorage/bin/activate
