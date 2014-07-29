b39ba914c22ba578eca91480deb98418babaac1d
Eduard Biceri-Matei <eduard.matei@cloudfounders.com>
2014-07-29 12:03:50 +0300

    Minor fix for check method for ovs rabbitmq.

---
+++ ovs/dal/hybrids/vpool.py
@@ -0,0 +1,70 @@
+# Copyright 2014 CloudFounders NV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+VPool module
+"""
+from ovs.dal.dataobject import DataObject
+from ovs.dal.structures import Dynamic, Property
+from ovs.extensions.storageserver.storagedriver import StorageDriverClient
+import time
+
+
+class VPool(DataObject):
+    """
+    The VPool class represents a vPool. A vPool is a Virtual Storage Pool, a Filesystem, used to
+    deploy vMachines. a vPool can span multiple Storage Drivers and connects to a single Storage Backend.
+    """
+    __properties = [Property('name', str, doc='Name of the vPool'),
+                    Property('description', str, mandatory=False, doc='Description of the vPool'),
+                    Property('size', int, mandatory=False, doc='Size of the vPool expressed in Bytes. Set to zero if not applicable.'),
+                    Property('login', str, mandatory=False, doc='Login/Username for the Storage Backend.'),
+                    Property('password', str, mandatory=False, doc='Password for the Storage Backend.'),
+                    Property('connection', str, mandatory=False, doc='Connection (IP, URL, Domainname, Zone, ...) for the Storage Backend.'),
+                    Property('type', ['CEPH_S3', 'AMAZON_S3', 'SWIFT_S3', 'LOCAL', 'DISTRIBUTED'], mandatory=False, doc='Type of the Storage Backend.'),
+                    Property('metadata', dict, mandatory=False, doc='Metadata for the backend, as used by the Storage Drivers.')]
+    __relations = []
+    __dynamics = [Dynamic('status',      str, 10),
+                  Dynamic('statistics',  dict, 5),
+                  Dynamic('stored_data', int, 60)]
+
+    def _status(self):
+        """
+        Fetches the Status of the vPool.
+        """
+        _ = self
+        return None
+
+    def _statistics(self):
+        """
+        Aggregates the Statistics (IOPS, Bandwidth, ...) of each vDisk served by the vPool.
+        """
+        client = StorageDriverClient()
+        vdiskstatsdict = {}
+        for key in client.stat_keys:
+            vdiskstatsdict[key] = 0
+            vdiskstatsdict['{0}_ps'.format(key)] = 0
+        for vdisk in self.vdisks:
+            vdisk.invalidate_dynamics('statistics')  # Prevent double caching
+            for key, value in vdisk.statistics.iteritems():
+                if key != 'timestamp':
+                    vdiskstatsdict[key] += value
+        vdiskstatsdict['timestamp'] = time.time()
+        return vdiskstatsdict
+
+    def _stored_data(self):
+        """
+        Aggregates the Stored Data of each vDisk served by the vPool.
+        """
+        return sum([disk.info['stored'] for disk in self.vdisks])
---
+++ ovs/extensions/hypervisor/hypervisors/vmware.py
@@ -0,0 +1,200 @@
+# Copyright 2014 CloudFounders NV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Module for the VMware hypervisor client
+"""
+
+import os
+from ovs.extensions.hypervisor.apis.vmware.sdk import Sdk
+
+
+class VMware(object):
+    """
+    Represents the hypervisor client for VMware
+    """
+
+    def __init__(self, ip, username, password):
+        """
+        Initializes the object with credentials and connection information
+        """
+        self.sdk = Sdk(ip, username, password)
+        self.state_mapping = {'poweredOn' : 'RUNNING',
+                              'poweredOff': 'HALTED',
+                              'suspended' : 'PAUSED'}
+
+    def get_state(self, vmid):
+        """
+        Get the current power state of a virtual machine
+        @param vmid: hypervisor id of the virtual machine
+        """
+        return self.state_mapping[self.sdk.get_power_state(vmid)]
+
+    def create_vm_from_template(self, name, source_vm, disks, ip, mountpoint, wait=True):
+        """
+        Create a new vmachine from an existing template
+        """
+        task = self.sdk.create_vm_from_template(name, source_vm, disks, ip, mountpoint, wait)
+        if wait is True:
+            if self.sdk.validate_result(task):
+                task_info = self.sdk.get_task_info(task)
+                return task_info.info.result.value
+        return None
+
+    def clone_vm(self, vmid, name, disks, wait=False):
+        """
+        Clone a vmachine
+
+        @param vmid: hypervisor id of the virtual machine
+        @param name: name of the virtual machine
+        @param disks: list of disk information
+        @param wait: wait for action to complete
+        """
+        task = self.sdk.clone_vm(vmid, name, disks, wait)
+        if wait is True:
+            if self.sdk.validate_result(task):
+                task_info = self.sdk.get_task_info(task)
+                return task_info.info.result.value
+        return None
+
+    def delete_vm(self, vmid, storagedriver_mountpoint, storagedriver_storage_ip, devicename, disks_info=None, wait=False):
+        """
+        Remove the vmachine from the hypervisor
+
+        @param vmid: hypervisor id of the virtual machine
+        @param wait: wait for action to complete
+        """
+        if disks_info is None:
+            disks_info = []
+        _ = disks_info
+        self.sdk.delete_vm(vmid, storagedriver_mountpoint, storagedriver_storage_ip, devicename, wait)
+
+    def get_vm_object(self, vmid):
+        """
+        Gets the VMware virtual machine object from VMware by its identifier
+        """
+        return self.sdk.get_vm(vmid)
+
+    def get_vm_agnostic_object(self, vmid):
+        """
+        Gets the VMware virtual machine object from VMware by its identifier
+        """
+        return self.sdk.make_agnostic_config(self.sdk.get_vm(vmid))
+
+    def get_vm_object_by_devicename(self, devicename, ip, mountpoint):
+        """
+        Gets the VMware virtual machine object from VMware by devicename
+        and datastore identifiers
+        """
+        return self.sdk.make_agnostic_config(self.sdk.get_nfs_datastore_object(ip, mountpoint, devicename)[0])
+
+    def get_vms_by_nfs_mountinfo(self, ip, mountpoint):
+        """
+        Gets a list of agnostic vm objects for a given ip and mountpoint
+        """
+        for vm in self.sdk.get_vms(ip, mountpoint):
+            yield self.sdk.make_agnostic_config(vm)
+
+    def is_datastore_available(self, ip, mountpoint):
+        """
+        @param ip : hypervisor ip to query for datastore presence
+        @param mountpoint: nfs mountpoint on hypervisor
+        @rtype: boolean
+        @return: True | False
+        """
+        return self.sdk.is_datastore_available(ip, mountpoint)
+
+    def set_as_template(self, vmid, disks, wait=False):
+        """
+        Configure a vm as template
+        This lets the machine exist on the hypervisor but configures
+        all disks as "Independent Non-persistent"
+
+        @param vmid: hypervisor id of the virtual machine
+        """
+        return self.sdk.set_disk_mode(vmid, disks, 'independent_nonpersistent', wait)
+
+    def mount_nfs_datastore(self, name, remote_host, remote_path):
+        """
+        Mounts a given NFS export as a datastore
+        """
+        return self.sdk.mount_nfs_datastore(name, remote_host, remote_path)
+
+    def test_connection(self):
+        """
+        Checks whether this node is a vCenter
+        """
+        return self.sdk.test_connection()
+
+    def clean_backing_disk_filename(self, path):
+        """
+        Cleans a backing disk filename to the corresponding disk filename
+        """
+        _ = self
+        return path.replace('-flat.vmdk', '.vmdk').strip('/')
+
+    def get_backing_disk_path(self, machinename, devicename):
+        """
+        Builds the path for the file backing a given device/disk
+        """
+        _ = self
+        return '/{}/{}-flat.vmdk'.format(machinename.replace(' ', '_'), devicename)
+
+    def get_disk_path(self, machinename, devicename):
+        """
+        Builds the path for the file backing a given device/disk
+        """
+        _ = self
+        return '/{}/{}.vmdk'.format(machinename.replace(' ', '_'), devicename)
+
+    def clean_vmachine_filename(self, path):
+        """
+        Cleans a VM filename
+        """
+        _ = self
+        return path.strip('/')
+
+    def get_vmachine_path(self, machinename, storagerouter_machineid):
+        """
+        Builds the path for the file representing a given vmachine
+        """
+        _ = self, storagerouter_machineid  # For compatibility purposes only
+        machinename = machinename.replace(' ', '_')
+        return '/{}/{}.vmx'.format(machinename, machinename)
+
+    def get_rename_scenario(self, old_name, new_name):
+        """
+        Gets the rename scenario based on the old and new name
+        """
+        _ = self
+        if old_name.endswith('.vmx') and new_name.endswith('.vmx'):
+            return 'RENAME'
+        elif old_name.endswith('.vmx~') and new_name.endswith('.vmx'):
+            return 'UPDATE'
+        return 'UNSUPPORTED'
+
+    def should_process(self, devicename):
+        """
+        Checks whether a given device should be processed
+        """
+        _ = self, devicename
+        return True
+
+    def file_exists(self, vpool, devicename):
+        """
+        Check if devicename exists on the given vpool
+        """
+        _ = self
+        filename = '/mnt/{0}/{1}'.format(vpool.name, devicename)
+        return os.path.exists(filename) and os.path.isfile(filename)
