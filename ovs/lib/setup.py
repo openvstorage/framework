598e7bb319f7c0c75316216a6268ee5ba73df15a
Kenneth Henderick <kenneth@ketronic.be>
2014-09-25 14:53:33 +0200

    Merge default into feature branch

--- ovs/lib/setup.py
+++ ovs/lib/setup.py
@@ -44,7 +44,7 @@ class SetupController(object):
     This class contains all logic for setting up an environment, installed with system-native packages
     """
 
-    PARTITION_DEFAULTS = {'device': 'DIR_ONLY', 'percentage': 50, 'label': 'cache1'}
+    PARTITION_DEFAULTS = {'device': 'DIR_ONLY', 'percentage': 'NA', 'label': 'cache1'}
 
     # Arakoon
     arakoon_client_config = '/opt/OpenvStorage/config/arakoon/{0}/{0}_client.cfg'
@@ -1611,11 +1611,11 @@ print blk_devices
 
         """
 
-        mountpoints_to_allocate = {'/mnt/md': {'device': 'DIR_ONLY', 'percentage': 25, 'label': 'mdpath'},
-                                   '/mnt/db': {'device': 'DIR_ONLY', 'percentage': 25, 'label': 'db'},
+        mountpoints_to_allocate = {'/mnt/md': {'device': 'DIR_ONLY', 'percentage': 'NA', 'label': 'mdpath'},
+                                   '/mnt/db': {'device': 'DIR_ONLY', 'percentage': 'NA', 'label': 'db'},
                                    '/mnt/cache1': dict(SetupController.PARTITION_DEFAULTS),
-                                   '/mnt/bfs': {'device': 'DIR_ONLY', 'percentage': 80, 'label': 'backendfs'},
-                                   '/var/tmp': {'device': 'DIR_ONLY', 'percentage': 20, 'label': 'tempfs'}}
+                                   '/mnt/bfs': {'device': 'DIR_ONLY', 'percentage': 'NA', 'label': 'backendfs'},
+                                   '/var/tmp': {'device': 'DIR_ONLY', 'percentage': 'NA', 'label': 'tempfs'}}
 
         selected_devices = dict(blk_devices)
         skipped_devices = set()
@@ -1646,7 +1646,9 @@ print blk_devices
 
         if nr_of_disks == 1:
             mountpoints_to_allocate['/var/tmp']['device'] = disk_devices[0]
+            mountpoints_to_allocate['/var/tmp']['percentage'] = 20
             mountpoints_to_allocate['/mnt/bfs']['device'] = disk_devices[0]
+            mountpoints_to_allocate['/mnt/bfs']['percentage'] = 80
 
         elif nr_of_disks >= 2:
             mountpoints_to_allocate['/var/tmp']['device'] = disk_devices[0]
@@ -1675,7 +1677,9 @@ print blk_devices
                 mountpoints_to_allocate[marker]['percentage'] = cache_size
 
             mountpoints_to_allocate['/mnt/md']['device'] = ssd_devices[0]
+            mountpoints_to_allocate['/mnt/md']['percentage'] = 25
             mountpoints_to_allocate['/mnt/db']['device'] = ssd_devices[1]
+            mountpoints_to_allocate['/mnt/db']['percentage'] = 25
 
         return mountpoints_to_allocate, skipped_devices
 
@@ -1692,6 +1696,13 @@ print blk_devices
             if mp in mounted:
                 print 'Unmounting {0}'.format(mp)
                 client.run('umount {0}'.format(mp))
+
+        mounted_devices = [device.strip() for device in client.run("cat /etc/mtab | cut -d ' ' -f 1").strip().split('\n')]
+        for mounted_device in mounted_devices:
+            for chosen_device in unique_disks:
+                if chosen_device in mounted_device:
+                    print 'Unmounting {0}'.format(mounted_device)
+                    client.run('umount {0}'.format(mounted_device))
 
         # wipe disks
         for disk in unique_disks:
@@ -1780,11 +1791,15 @@ print blk_devices
                 sub_keys = proposed[mp].keys()
                 sub_keys.sort()
                 mp_values = ''
+                if not proposed[mp]['device'] or proposed[mp]['device'] in ['DIR_ONLY']:
+                    mp_values = ' {0} : {1:20}'.format('device', 'DIR_ONLY')
+                    print "{0:20} : {1}".format(mp, mp_values)
+                    key_map.append(mp)
+                    continue
+
                 for sub_key in sub_keys:
                     value = str(proposed[mp][sub_key])
-                    if sub_key == 'device' and not value:
-                        value = 'DIR_ONLY'
-                    elif sub_key == 'device' and value and value != 'DIR_ONLY':
+                    if sub_key == 'device' and value and value != 'DIR_ONLY':
                         size = device_size_map[value]
                         size_in_gb = int(size / 1000.0 / 1000.0 / 1000.0)
                         value = value + ' ({0} GB)'.format(size_in_gb)
@@ -1810,7 +1825,7 @@ print blk_devices
             print
 
         def is_device_path(value):
-            if re.match('/dev/[a-z]3', value):
+            if re.match('/dev/[a-z][a-z][a-z]+', value):
                 return True
             else:
                 return False
@@ -1823,11 +1838,11 @@ print blk_devices
 
         def is_percentage(value):
             try:
-                if int(value) >= 0 and int(value) <=100:
+                if 0 <= int(value) <= 100:
                     return True
                 else:
                     return False
-            except:
+            except ValueError:
                 return False
 
         def is_label(value):
@@ -1851,8 +1866,14 @@ print blk_devices
         def _summarize_partition_percentages(layout):
             total = dict()
             for details in layout.itervalues():
-                percentage = int(details['percentage'])
                 device = details['device']
+                if device == 'DIR_ONLY':
+                    continue
+                if details['percentage'] == 'NA':
+                    print '>>> Invalid value {0}% for device: {1}'.format(details['percentage'], device)
+                    time.sleep(1)
+                    return False
+                percentage = int(details['percentage'])
                 if device in total:
                     total[device] += percentage
                 else:
@@ -1937,6 +1958,9 @@ print blk_devices
                 elif chosen == 'Apply':
                     if not _summarize_partition_percentages(default):
                         'Partition totals are not within percentage range'
+                        choices = show_layout(default)
+                        continue
+
                     show_layout(default)
                     confirmation = choice.Input('Please confirm partition layout (yes/no), ALL DATA WILL BE ERASED ON THE DISKS ABOVE!', str).ask()
                     if confirmation.lower() == 'yes':
