1a0f76d61c679bfe1bad366512678efaa2520257
Kenneth Henderick <kenneth@ketronic.be>
2014-12-16 13:11:27 +0100

    Merge default into product-management

---
+++ webapps/api/backend/views/roles.py
@@ -0,0 +1,52 @@
+# Copyright 2014 CloudFounders NV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Module for roles
+"""
+
+from backend.decorators import required_roles, return_object, return_list, load, log
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from ovs.dal.hybrids.role import Role
+from ovs.dal.lists.rolelist import RoleList
+
+
+class RoleViewSet(viewsets.ViewSet):
+    """
+    Information about Roles
+    """
+    permission_classes = (IsAuthenticated,)
+    prefix = r'roles'
+    base_name = 'roles'
+
+    @log()
+    @required_roles(['read'])
+    @return_list(Role)
+    @load()
+    def list(self):
+        """
+        Lists all available Roles
+        """
+        return RoleList.get_roles()
+
+    @log()
+    @required_roles(['read'])
+    @return_object(Role)
+    @load(Role)
+    def retrieve(self, role):
+        """
+        Load information about a given Role
+        """
+        return role
---
+++ webapps/frontend/app/viewmodels/containers/storagedriver.js
@@ -0,0 +1,77 @@
+// Copyright 2014 CloudFounders NV
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/*global define */
+define([
+    'jquery', 'knockout',
+    'ovs/api'
+], function($, ko, api) {
+    "use strict";
+    return function(guid) {
+        var self = this;
+
+        // Handles
+        self.loadHandle = undefined;
+
+        // Observables
+        self.loading              = ko.observable(false);
+        self.loaded               = ko.observable(false);
+        self.guid                 = ko.observable(guid);
+        self.name                 = ko.observable();
+        self.ports                = ko.observableArray([0, 0, 0]);
+        self.clusterIP            = ko.observable();
+        self.storageIP            = ko.observable();
+        self.storageDriverID      = ko.observable();
+        self.mountpoint           = ko.observable();
+        self.mountpointTemp       = ko.observable();
+        self.mountpointBFS        = ko.observable();
+        self.mountpointMD         = ko.observable();
+        self.mountpointReadCache1 = ko.observable();
+        self.mountpointReadCache2 = ko.observable();
+        self.mountpointWriteCache = ko.observable();
+        self.mountpointFOC        = ko.observable();
+
+        // Functions
+        self.fillData = function(data) {
+            self.name(data.name);
+            self.ports(data.ports);
+            self.clusterIP(data.cluster_ip);
+            self.storageIP(data.storage_ip);
+            self.storageDriverID(data.storageDriverID);
+            self.mountpoint(data.mountpoint);
+            self.mountpointTemp(data.mountpoint_temp);
+            self.mountpointBFS(data.mountpoint_bfs);
+            self.mountpointMD(data.mountpoint_md);
+            self.mountpointReadCache1(data.mountpoint_readcache1);
+            self.mountpointReadCache2(data.mountpoint_readcache2);
+            self.mountpointWriteCache(data.mountpoint_writecache);
+            self.mountpointFOC(data.mountpoint_foc);
+            self.loaded(true);
+            self.loading(false);
+        };
+        self.load = function() {
+            return $.Deferred(function(deferred) {
+                self.loading(true);
+                api.get('storagedrivers/' + self.guid())
+                    .done(function(data) {
+                        self.fillData(data);
+                        deferred.resolve();
+                    })
+                    .fail(deferred.reject)
+                    .always(function() {
+                        self.loading(false);
+                    });
+            }).promise();
+        }
+    };
+});
