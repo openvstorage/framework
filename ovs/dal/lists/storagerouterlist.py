2ca1dc3dd5c73c4fcca127cb1105f4320d58f07e
Eduard Biceri-Matei <eduard.matei@cloudfounders.com>
2015-02-25 16:55:19 +0200

    OVS-1690 Arakoon option -catchup_only is not used correctly - fixed usage of option, to be used on demand not on start

---
+++ ovs/dal/lists/storagerouterlist.py
@@ -0,0 +1,91 @@
+# Copyright 2014 CloudFounders NV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+StorageRouterList module
+"""
+from ovs.dal.datalist import DataList
+from ovs.dal.dataobjectlist import DataObjectList
+from ovs.dal.hybrids.storagerouter import StorageRouter
+
+
+class StorageRouterList(object):
+    """
+    This StorageRouterList class contains various lists regarding to the StorageRouter class
+    """
+
+    @staticmethod
+    def get_storagerouters():
+        """
+        Returns a list of all StorageRouters
+        """
+        storagerouters = DataList({'object': StorageRouter,
+                                   'data': DataList.select.GUIDS,
+                                   'query': {'type': DataList.where_operator.AND,
+                                             'items': []}}).data
+        return DataObjectList(storagerouters, StorageRouter)
+
+    @staticmethod
+    def get_slaves():
+        """
+        Get all SLAVE StorageRouters
+        """
+        storagerouters = DataList({'object': StorageRouter,
+                                   'data': DataList.select.GUIDS,
+                                   'query': {'type': DataList.where_operator.AND,
+                                             'items': [('node_type', DataList.operator.EQUALS, 'EXTRA')]}}).data
+        return DataObjectList(storagerouters, StorageRouter)
+
+    @staticmethod
+    def get_masters():
+        """
+        Get all MASTER StorageRouters
+        """
+        storagerouters = DataList({'object': StorageRouter,
+                                   'data': DataList.select.GUIDS,
+                                   'query': {'type': DataList.where_operator.AND,
+                                             'items': [('node_type', DataList.operator.EQUALS, 'MASTER')]}}).data
+        return DataObjectList(storagerouters, StorageRouter)
+
+    @staticmethod
+    def get_by_machine_id(machine_id):
+        """
+        Returns a StorageRouter by its machine_id
+        """
+        storagerouters = DataList({'object': StorageRouter,
+                                   'data': DataList.select.GUIDS,
+                                   'query': {'type': DataList.where_operator.AND,
+                                             'items': [('machine_id', DataList.operator.EQUALS, machine_id)]}}).data
+        srs = DataObjectList(storagerouters, StorageRouter)
+        if len(srs) == 0:
+            return None
+        if len(srs) == 1:
+            return srs[0]
+        raise RuntimeError('There should be only one StorageRouter with machine_id: {0}'.format(machine_id))
+
+    @staticmethod
+    def get_by_ip(ip):
+        """
+        Returns a StorageRouter by its ip
+        """
+        storagerouters = DataList({'object': StorageRouter,
+                                   'data': DataList.select.GUIDS,
+                                   'query': {'type': DataList.where_operator.AND,
+                                             'items': [('ip', DataList.operator.EQUALS, ip)]}}).data
+        srs = DataObjectList(storagerouters, StorageRouter)
+        if len(srs) == 0:
+            return None
+        if len(srs) == 1:
+            return srs[0]
+        raise RuntimeError('There should be only one StorageRouter with ip: {0}'.format(ip))
---
+++ ovs/extensions/api/client.py
@@ -0,0 +1,86 @@
+# Copyright 2015 CloudFounders NV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Module for the OVS API client
+"""
+
+import urllib2
+import urllib
+import json
+import base64
+import time
+
+
+class OVSClient(object):
+    """
+    Represents the OVS client
+    """
+
+    def __init__(self, ip, port, client_id, client_secret):
+        """
+        Initializes the object with credentials and connection information
+        """
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self._url = 'https://{0}:{1}/api'.format(ip, port)
+        self._token = None
+
+    def _connect(self):
+        """
+        Authenticates to the api
+        """
+        headers = {'Accept': 'application/json',
+                   'Authorization': 'Basic {0}'.format(base64.b64encode('{0}:{1}'.format(self.client_id, self.client_secret)).strip())}
+        request = urllib2.Request('{0}/oauth2/token/'.format(self._url),
+                                  data=urllib.urlencode({'grant_type': 'client_credentials'}),
+                                  headers=headers)
+        response = urllib2.urlopen(request).read()
+        response_data = json.loads(response)
+        self._token = response_data['access_token']
+
+    def call(self, api, post_data=None, get_data=None):
+        """
+        Executes an API call
+        """
+        if self._token is None:
+            self._connect()
+
+        headers = {'Accept': 'application/json; version=1',
+                   'Authorization': 'Bearer {0}'.format(self._token)}
+        url_params = ''
+        if get_data is not None:
+            url_params = '?{0}'.format(urllib.urlencode(get_data))
+        request = urllib2.Request('{0}{1}{2}'.format(self._url, api, url_params),
+                                  data=post_data,
+                                  headers=headers)
+        response = urllib2.urlopen(request).read()
+        return json.loads(response)
+
+    def wait_for_task(self, task_id, timeout=None):
+        """
+        Waits for a task to complete
+        """
+        if self._token is None:
+            self._connect()
+
+        start = time.time()
+        task_metadata = {'ready': False}
+        while task_metadata['ready'] is False:
+            if timeout is not None and timeout < (time.time() - start):
+                raise RuntimeError('Waiting for task {0} has timed out.'.format(task_id))
+            task_metadata = self.call('/tasks/{0}/'.format(task_id))
+            if task_metadata['ready'] is False:
+                time.sleep(1)
+        return task_metadata['successful'], task_metadata['result']
