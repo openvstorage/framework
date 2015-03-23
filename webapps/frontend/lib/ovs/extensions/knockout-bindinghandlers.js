ebbe37f12ae7b4390f64586fc72123eb0c793933
Kenneth Henderick <kenneth@ketronic.be>
2015-03-23 11:31:29 +0100

    OVS-1881 - Make sure the pie chart updates

--- webapps/frontend/lib/ovs/extensions/knockout-bindinghandlers.js
+++ webapps/frontend/lib/ovs/extensions/knockout-bindinghandlers.js
@@ -206,66 +206,100 @@ define(['knockout', 'jquery', 'd3', 'ovs/generic'], function(ko, $, d3, generic)
     ko.virtualElements.allowedBindings['let'] = true;
     ko.bindingHandlers.pie = {
         init: function(element) {
-            var id = 'id_' + generic.getTimestamp() + '_' + Math.random().toString().substr(2, 10), height = 200;
+            var id = 'id_' + generic.getTimestamp() + '_' + Math.random().toString().substr(2, 10), size = 200, svg;
             $(element).html('<div></div>');
             $($(element).children()[0]).attr('id', id);
-            d3.select('#' + id).append('svg')
+            svg = d3.select('#' + id).append('svg')
                 .attr('class', 'svg')
-                .attr('width', height * 2.5)
-                .attr('height', height)
-                .append('g')
+                .attr('width', size * 2.5)
+                .attr('height', size);
+            svg.append('g')
                 .attr('class', 'container')
-                .attr('transform', 'translate(' + height / 2 + ',' + height / 2 + ')');
+                .attr('transform', 'translate(' + size / 2 + ',' + size / 2 + ')');
+            svg.append('g')
+                .attr('class', 'legendcontainer')
+                .attr('transform', 'translate(' + (size + 20) + ',0)');
         },
         update: function(element, valueAccessor) {
-            var id, g, pie, arc, legend, size = 200, data = valueAccessor(),
+            var id, g, pie, path, arc, legend, entry, size = 200, data = valueAccessor(),
                 color = d3.scale.ordinal().range([
                     '#e6e6e6', '#b2b2b2', '#808080',
                     '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'
                 ]);
+
             arc = d3.svg.arc()
                 .outerRadius(size / 2)
                 .innerRadius(0);
             pie = d3.layout.pie()
                 .sort(null)
                 .value(function(d) { return d.value; });
+
             id = $($(element).children()[0]).attr('id');
-            g = d3.select('#' + id).select('.container')
-                .selectAll('.arc')
-                .data(pie(data))
-                .enter()
-                .append('g')
-                .attr('class', 'arc');
-            g.append('path')
+
+            g = d3.select('#' + id).select('.container');
+            g.datum(data).selectAll('path')
+                .data(pie, function(d) { return d.data.name; })
+                .style('fill', function(d) { return color(d.data.name); })
+                .transition()
+                .duration(250)
+                .attrTween('d', function(a) {
+                    var i = d3.interpolate(this._current, a);
+                    this._current = i(0);
+                    return function(t) {
+                        return arc(i(t));
+                    };
+                });
+            g.datum(data).selectAll('path')
+                .data(pie, function(d) { return d.data.name; })
+                .enter().append('path')
+                .style('fill', function(d) { return color(d.data.name); })
                 .attr('d', arc)
-                .style('fill', function(d) {
-                    var text = d.data.name + ' (' + generic.formatBytes(d.data.value);
-                    if (d.data.hasOwnProperty('percentage')) {
-                        text += ' - ' + generic.formatPercentage(d.data.percentage);
+                .each(function(d) { this._current = d; });
+            g.datum(data).selectAll('path')
+                .data(pie, function(d) { return d.data.name; })
+                .exit().remove();
+
+            legend = d3.select('#' + id).select('.legendcontainer');
+            legend.selectAll('g')
+                .data(data, function(d) { return d.name; })
+                .select('text')
+                .text(function(d) {
+                    var text = d.name + ' (' + generic.formatBytes(d.value);
+                    if (d.hasOwnProperty('percentage')) {
+                        text += ' - ' + generic.formatPercentage(d.percentage);
                     }
                     text += ')';
-                    return color(text);
+                    return text;
                 });
-            legend = d3.select('#' + id).select('.svg').selectAll('.legend')
-                .data(color.domain())
+            entry = legend.selectAll('g')
+                .data(data, function(d) { return d.name; })
                 .enter()
                 .append('g')
                 .attr('class', 'legend')
                 .attr('transform', function(d, i) {
                     var height = 25,
-                        horz = 220,
                         vert = i * height;
-                    return 'translate(' + horz + ',' + vert + ')';
+                    return 'translate(0,' + vert + ')';
                 });
-            legend.append('rect')
+            entry.append('rect')
                 .attr('width', 15)
                 .attr('height', 15)
-                .style('fill', color)
-                .style('stroke', color);
-            legend.append('text')
+                .style('fill', function(d) { return color(d.name); })
+                .style('stroke', function(d) { return color(d.name); });
+            entry.append('text')
                 .attr('x', 20)
                 .attr('y', 14)
-                .text(function(d) { return d; });
+                .text(function(d) {
+                    var text = d.name + ' (' + generic.formatBytes(d.value);
+                    if (d.hasOwnProperty('percentage')) {
+                        text += ' - ' + generic.formatPercentage(d.percentage);
+                    }
+                    text += ')';
+                    return text;
+                });
+            legend.selectAll('g')
+                .data(data, function(d) { return d.name; })
+                .exit().remove();
         }
     };
 });
