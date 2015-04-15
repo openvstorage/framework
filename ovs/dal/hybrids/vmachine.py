74792207178ccb5a5814c2b10a22adb1cb5c8ba6
kvanhijf <kevin.vanhijfte@openvstorage.com>
2015-04-15 18:01:02 +0200

    OVS-2028 - Added mds checkup after removal of vpool

---
+++ ovs/dal/hybrids/vmachine.py
@@ -0,0 +1,147 @@
+# Copyright 2014 CloudFounders NV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+VMachine module
+"""
+from ovs.dal.dataobject import DataObject
+from ovs.dal.structures import Property, Relation, Dynamic
+from ovs.dal.datalist import DataList
+from ovs.dal.dataobjectlist import DataObjectList
+from ovs.dal.hybrids.pmachine import PMachine
+from ovs.dal.hybrids.vpool import VPool
+from ovs.extensions.storageserver.storagedriver import StorageDriverClient
+from ovs.extensions.hypervisor.factory import Factory as hvFactory
+import time
+
+
+class VMachine(DataObject):
+    """
+    The VMachine class represents a vMachine. A vMachine is a Virtual Machine with vDisks
+    or a Virtual Machine running the Open vStorage software.
+    """
+    __properties = [Property('name', str, mandatory=False, doc='Name of the vMachine.'),
+                    Property('description', str, mandatory=False, doc='Description of the vMachine.'),
+                    Property('hypervisor_id', str, mandatory=False, doc='The identifier of the vMachine on the Hypervisor.'),
+                    Property('devicename', str, doc='The name of the container file (e.g. the VMX-file) describing the vMachine.'),
+                    Property('is_vtemplate', bool, default=False, doc='Indicates whether this vMachine is a vTemplate.'),
+                    Property('status', ['OK', 'NOK', 'CREATED', 'SYNC', 'SYNC_NOK'], default='OK', doc='Internal status of the vMachine')]
+    __relations = [Relation('pmachine', PMachine, 'vmachines'),
+                   Relation('vpool', VPool, 'vmachines', mandatory=False)]
+    __dynamics = [Dynamic('snapshots', list, 60),
+                  Dynamic('hypervisor_status', str, 300),
+                  Dynamic('statistics', dict, 0),
+                  Dynamic('stored_data', int, 60),
+                  Dynamic('failover_mode', str, 60),
+                  Dynamic('storagerouters_guids', list, 15),
+                  Dynamic('vpools_guids', list, 15)]
+
+    def _snapshots(self):
+        """
+        Fetches a list of Snapshots for the vMachine.
+        """
+        snapshots_structure = {}
+        for disk in self.vdisks:
+            for snapshot in disk.snapshots:
+                timestamp = snapshot['timestamp']
+                if timestamp not in snapshots_structure:
+                    snapshots_structure[timestamp] = {'label': snapshot['label'],
+                                                      'is_consistent': snapshot['is_consistent'],
+                                                      'is_automatic': snapshot.get('is_automatic', True),
+                                                      'stored': 0,
+                                                      'snapshots': {}}
+                snapshots_structure[timestamp]['snapshots'][disk.guid] = snapshot['guid']
+                snapshots_structure[timestamp]['stored'] = snapshots_structure[timestamp]['stored'] + snapshot['stored']
+
+        snapshots = []
+        for timestamp in sorted(snapshots_structure.keys()):
+            item = snapshots_structure[timestamp]
+            snapshots.append({'timestamp': timestamp,
+                              'label': item['label'],
+                              'is_consistent': item['is_consistent'],
+                              'is_automatic': item.get('is_automatic', True),
+                              'stored': item['stored'],
+                              'snapshots': item['snapshots']})
+        return snapshots
+
+    def _hypervisor_status(self):
+        """
+        Fetches the Status of the vMachine.
+        """
+        if self.hypervisor_id is None or self.pmachine is None:
+            return 'UNKNOWN'
+        hv = hvFactory.get(self.pmachine)
+        try:
+            return hv.get_state(self.hypervisor_id)
+        except:
+            return 'UNKNOWN'
+
+    def _statistics(self):
+        """
+        Aggregates the Statistics (IOPS, Bandwidth, ...) of each vDisk of the vMachine.
+        """
+        vdiskstatsdict = {}
+        for key in StorageDriverClient.stat_keys:
+            vdiskstatsdict[key] = 0
+            vdiskstatsdict['{0}_ps'.format(key)] = 0
+        for vdisk in self.vdisks:
+            for key, value in vdisk.statistics.iteritems():
+                if key != 'timestamp':
+                    vdiskstatsdict[key] += value
+        vdiskstatsdict['timestamp'] = time.time()
+        return vdiskstatsdict
+
+    def _stored_data(self):
+        """
+        Aggregates the Stored Data of each vDisk of the vMachine.
+        """
+        return sum([vdisk.info['stored'] for vdisk in self.vdisks])
+
+    def _failover_mode(self):
+        """
+        Gets the aggregated failover mode
+        """
+        status = 'UNKNOWN'
+        status_code = 0
+        for vdisk in self.vdisks:
+            mode = vdisk.info['failover_mode']
+            current_status_code = StorageDriverClient.foc_status[mode.lower()]
+            if current_status_code > status_code:
+                status = mode
+                status_code = current_status_code
+        return status
+
+    def _storagerouters_guids(self):
+        """
+        Gets the StorageRouter guids linked to this vMachine
+        """
+        storagerouter_guids = set()
+        from ovs.dal.hybrids.storagedriver import StorageDriver
+        storagedriver_ids = [vdisk.storagedriver_id for vdisk in self.vdisks if vdisk.storagedriver_id is not None]
+        storagedrivers = DataList({'object': StorageDriver,
+                                   'data': DataList.select.GUIDS,
+                                   'query': {'type': DataList.where_operator.AND,
+                                             'items': [('storagedriver_id', DataList.operator.IN, storagedriver_ids)]}}).data  # noqa
+        for storagedriver in DataObjectList(storagedrivers, StorageDriver):
+            storagerouter_guids.add(storagedriver.storagerouter_guid)
+        return list(storagerouter_guids)
+
+    def _vpools_guids(self):
+        """
+        Gets the vPool guids linked to this vMachine
+        """
+        vpool_guids = set()
+        for vdisk in self.vdisks:
+            vpool_guids.add(vdisk.vpool_guid)
+        return list(vpool_guids)
---
+++ webapps/frontend/app/viewmodels/wizards/addvpool/gather_mountpoints.js
@@ -0,0 +1,203 @@
+// Copyright 2014 CloudFounders NV
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/*global define */
+define([
+    'jquery', 'knockout', 'ovs/generic', './data'
+], function($, ko, generic, data) {
+    "use strict";
+    return function() {
+        var self = this;
+
+        // Variables
+        self.data = data;
+
+        // Computed
+        self.canContinue = ko.computed(function() {
+            var reasons = [], fields = [], storagedriver_mountpoints = [], bfs_mountpoints = [];
+
+            // Collect previously configured storagedriver mountpoints
+            $.each(self.data.storageDrivers(), function(index, storageDriver) {
+                if (self.data.target() !== undefined && storageDriver.storageDriverID() === (self.data.name() + self.data.target().machineId())) {
+                    return true;
+                }
+                if (storageDriver.mountpointBFS() !== undefined) {
+                    bfs_mountpoints.push(storageDriver.mountpointBFS());
+                    storagedriver_mountpoints.push(storageDriver.mountpointBFS());
+                }
+                if (storageDriver.mountpointMD() !== undefined) {
+                    storagedriver_mountpoints.push(storageDriver.mountpointMD());
+                }
+                if (storageDriver.mountpointFOC() !== undefined) {
+                    storagedriver_mountpoints.push(storageDriver.mountpointFOC());
+                }
+                if (storageDriver.mountpointTemp() !== undefined) {
+                    storagedriver_mountpoints.push(storageDriver.mountpointTemp());
+                }
+                if (storageDriver.mountpointReadCaches() !== undefined) {
+                    $.each(storageDriver.mountpointReadCaches(), function (i, e) {
+                        storagedriver_mountpoints.push(e.substring(0, e.lastIndexOf('/')));
+                    });
+                }
+                if (storageDriver.mountpointWriteCaches() !== undefined) {
+                    $.each(storageDriver.mountpointWriteCaches(), function (i, e) {
+                        storagedriver_mountpoints.push(e.substring(0, e.lastIndexOf('/')));
+                    });
+                }
+                return true;
+            });
+
+            // BFS mountpoint checks
+            if (self.data.backend() === 'local' || self.data.backend() === 'distributed') {
+                if (self.data.mtptBFS() !== '' && storagedriver_mountpoints.contains(self.data.mtptBFS())) {
+                    fields.push('bfs');
+                    reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.mtptinuse', {what: $.t('ovs:wizards.addvpool.gathermountpoints.bfsname')}));
+                }
+                else {
+                    $.each(bfs_mountpoints, function (index, mp) {
+                        if (self.data.mtptBFS() !== undefined && self.data.mtptBFS().startsWith(mp + '/')) {
+                            fields.push('bfs');
+                            reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.bfsnotshared'));
+                        }
+                    });
+                }
+                if (self.data.mtptBFS() !== '' &&
+                   (self.data.mtptBFS() === self.data.mtptMD() ||
+                    self.data.mtptBFS() === self.data.mtptTemp() ||
+                    self.data.mtptReadCaches().contains(self.data.mtptBFS()) ||
+                    self.data.mtptWriteCaches().contains(self.data.mtptBFS()))) {
+                        fields.push('bfs');
+                        reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.uniquemtpt', {what: $.t('ovs:wizards.addvpool.gathermountpoints.bfsname')}));
+                }
+                if (!self.data.mtptBFS.valid()) {
+                    fields.push('bfs');
+                    reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.invalidmtpt', { what: $.t('ovs:wizards.addvpool.gathermountpoints.bfsname') }));
+                }
+            }
+            else {
+                self.data.mtptBFS('/mnt/bfs'); // BFS isn't used, so set it to a non-conflicting value
+            }
+
+            // Temp mountpoint checks
+            if (self.data.mtptTemp() !== '' && storagedriver_mountpoints.contains(self.data.mtptTemp())) {
+                fields.push('temp');
+                reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.mtptinuse', {what: $.t('ovs:wizards.addvpool.gathermountpoints.tempname')}));
+            }
+            if (!self.data.mtptTemp.valid()) {
+                fields.push('temp');
+                reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.invalidmtpt', { what: $.t('ovs:wizards.addvpool.gathermountpoints.tempname') }));
+            }
+
+            // MD mountpoint checks
+            if (self.data.mtptMD() !== '' && storagedriver_mountpoints.contains(self.data.mtptMD())) {
+                fields.push('md');
+                reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.mtptinuse', {what: $.t('ovs:wizards.addvpool.gathermountpoints.mdname')}));
+            }
+            if (!self.data.mtptMD.valid()) {
+                fields.push('md');
+                reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.invalidmtpt', { what: $.t('ovs:wizards.addvpool.gathermountpoints.mdname') }));
+            }
+
+            // Read cache mountpoint checks
+            if (self.data.mtptReadCaches().length === 0) {
+                fields.push('readcache');
+                reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.atleastone', {what: $.t('ovs:wizards.addvpool.gathermountpoints.readcachename')}));
+            }
+            $.each(self.data.mtptReadCaches(), function (index, mp) {
+                if (storagedriver_mountpoints.contains(mp)) {
+                    fields.push('readcache');
+                    reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.mtptinuse', {what: $.t('ovs:wizards.addvpool.gathermountpoints.readcachename')}));
+                }
+                if (self.data.mtptBFS() === mp || self.data.mtptFOC() === mp || self.data.mtptMD() === mp || self.data.mtptTemp() === mp || self.data.mtptWriteCaches().contains(mp)) {
+                    fields.push('readcache');
+                    reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.uniquemtpt', {what: $.t('ovs:wizards.addvpool.gathermountpoints.readcachename')}));
+                }
+                if (mp.match(self.data.mountpointRegex) === null) {
+                    fields.push('readcache');
+                    reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.invalidmtpt', { what: $.t('ovs:wizards.addvpool.gathermountpoints.readcachename') }));
+                }
+            });
+
+            // Write cache mountpoint checks
+            if (self.data.mtptWriteCaches().length === 0) {
+                fields.push('writecache');
+                reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.atleastone', {what: $.t('ovs:wizards.addvpool.gathermountpoints.writecachename')}));
+            }
+            $.each(self.data.mtptWriteCaches(), function (index, mp) {
+                if (storagedriver_mountpoints.contains(mp)) {
+                    fields.push('writecache');
+                    reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.mtptinuse', {what: $.t('ovs:wizards.addvpool.gathermountpoints.writecachename')}));
+                }
+                if (mp.match(self.data.mountpointRegex) === null) {
+                    fields.push('writecache');
+                    reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.invalidmtpt', {what: $.t('ovs:wizards.addvpool.gathermountpoints.writecachename')}));
+                }
+            });
+
+            // FOC mountpoint checks
+            if (self.data.mtptFOC() !== '' && storagedriver_mountpoints.contains(self.data.mtptFOC())) {
+                fields.push('foc');
+                reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.mtptinuse', {what: $.t('ovs:wizards.addvpool.gathermountpoints.focname')}));
+            }
+            if (!self.data.mtptFOC.valid()) {
+                fields.push('foc');
+                reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.invalidmtpt', { what: $.t('ovs:wizards.addvpool.gathermountpoints.focname') }));
+            }
+
+            // vPool check
+            if (!self.data.allowVPool()) {
+                fields.push('vpool');
+                reasons.push($.t('ovs:wizards.addvpool.gathermountpoints.vpoolnotallowed'));
+            }
+            var valid = reasons.length === 0;
+            var unique_fields = fields.filter(generic.arrayFilterUnique);
+            var unique_reasons = reasons.filter(generic.arrayFilterUnique);
+            return { value: valid, reasons: unique_reasons, fields: unique_fields };
+        });
+
+        self.addReadCache = function() {
+            var value = self.data.mtptCustomRC();
+            if (value !== undefined && value !== '') {
+                if (!self.data.mtptCustomRCs().contains(value) && !self.data.mountpoints().contains(value)) {
+                    self.data.mtptCustomRCs.push(value);
+                    self.data.mtptReadCaches.push(value);
+                }
+                self.data.mtptCustomRC('');
+            }
+        };
+
+        self.addWriteCache = function() {
+            var value = self.data.mtptCustomWC();
+            if (value !== undefined && value !== '') {
+                if (!self.data.mtptCustomWCs().contains(value) && !self.data.mountpoints().contains(value)) {
+                    self.data.mtptCustomWCs.push(value);
+                    self.data.mtptWriteCaches.push(value);
+                }
+                self.data.mtptCustomWC('');
+            }
+        };
+
+        self.activate = function() {
+            if (self.data.readcaches().length >= 1) {
+                if (!self.data.mtptReadCaches().contains(self.data.readcaches()[0])) {
+                    self.data.mtptReadCaches.push(self.data.readcaches()[0]);
+                }
+            }
+            if (self.data.writecaches().length >= 1) {
+                if (!self.data.mtptWriteCaches().contains(self.data.writecaches()[0])) {
+                    self.data.mtptWriteCaches.push(self.data.writecaches()[0]);
+                }
+            }
+        }
+    };
+});
