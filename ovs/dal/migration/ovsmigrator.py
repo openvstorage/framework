ce7c5122cf1730805c75406e105ef1cd6bc2fee9
kvanhijf <kevin.vanhijfte@openvstorage.com>
2015-06-23 11:33:04 +0200

    OVS-2073: Migrator testcode

--- ovs/dal/migration/ovsmigrator.py
+++ ovs/dal/migration/ovsmigrator.py
@@ -167,6 +167,10 @@ class OVSMigrator(object):
         if working_version < 2:
             # Execute some code that upgrades to version 2
             # working_version = 2
-            pass
+            from ovs.dal.lists.storagerouterlist import StorageRouterList
+            sr = StorageRouterList.get_storagerouters()[0]
+            sr.name = sr.name.rstrip('-update')
+            sr.save()
+            working_version = 2
 
         return working_version
