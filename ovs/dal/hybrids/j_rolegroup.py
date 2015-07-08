5468350385c7ed532b0fd9a5a30985a960074a8b
Kenneth Henderick <kenneth@ketronic.be>
2015-07-08 13:54:17 +0200

    Updated License from CloudFounders NV to Open vStorage NV

--- ovs/dal/hybrids/j_rolegroup.py
+++ ovs/dal/hybrids/j_rolegroup.py
@@ -1,4 +1,4 @@
-# Copyright 2014 CloudFounders NV
+# Copyright 2014 Open vStorage NV
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
