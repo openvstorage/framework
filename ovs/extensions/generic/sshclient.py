9f5aa73fa79f3dbd75f2343df84b212d980678e3
Kenneth Henderick <kenneth@ketronic.be>
2015-09-17 18:22:56 +0200

    OVS-2988 - Handle more unhappy paths

--- ovs/extensions/generic/sshclient.py
+++ ovs/extensions/generic/sshclient.py
@@ -151,15 +151,19 @@ class SSHClient(object):
             return
 
         try:
-            self.client.connect(self.ip, username=self.username, password=self.password)
+            try:
+                self.client.connect(self.ip, username=self.username, password=self.password)
+            except:
+                try:
+                    self.client.close()
+                except:
+                    pass
+                raise
         except socket.error as ex:
             if 'No route to host' in str(ex):
                 message = 'SocketException: No route to host {0}'.format(self.ip)
                 logger.error(message)
                 raise UnableToConnectException(message)
-            raise
-        except paramiko.AuthenticationException:
-            self.client.close()
             raise
 
     def _disconnect(self):
