175b95cdf20c88f7f36ccf8f243b9f744672d723
kvanhijf <kevin.vanhijfte@openvstorage.com>
2015-10-13 16:57:51 +0200

    OVS-3255 - Fixed some unit tests Fixed issue in scrub delete snapshots Created new file which executes all unit tests

--- ovs/extensions/db/arakoon/tests/test_arakoonInstaller.py
+++ ovs/extensions/db/arakoon/tests/test_arakoonInstaller.py
@@ -14,8 +14,6 @@
 
 import sys
 from ovs.extensions.db.arakoon.ArakoonInstaller import ArakoonInstaller
-from ovs.extensions.storage.persistentfactory import PersistentFactory
-from ovs.extensions.generic.configuration import Configuration
 from ovs.extensions.generic.system import System
 from ovs.extensions.generic.sshclient import SSHClient
 from unittest import TestCase
@@ -26,91 +24,64 @@ class TestArakoonInstaller(TestCase):
     nodes = None
     expected_global = None
     expected_base = None
+    cluster_name = None
+    cluster_config_file = None
+    cluster_config_path = None
 
     @classmethod
     def setUpClass(cls):
-        ArakoonInstaller.ARAKOON_CONFIG_DIR = '/tmp/cfg'
-        ArakoonInstaller.ARAKOON_CONFIG_FILE = '/tmp/cfg/{0}/{0}.cfg'
+        TestArakoonInstaller.cluster_name = 'unit_test_cluster'
+        TestArakoonInstaller.cluster_config_path = '/opt/OpenvStorage/config/arakoon/{0}/'.format(TestArakoonInstaller.cluster_name)
+        TestArakoonInstaller.cluster_config_file = '/opt/OpenvStorage/config/arakoon/{0}/{0}.cfg'.format(TestArakoonInstaller.cluster_name)
+        TestArakoonInstaller.expected_global = '[global]\ncluster = {0}\ncluster_id = {1}\nplugins =\n\n'
+        TestArakoonInstaller.expected_base = '[{0}]\ntlog_compression = snappy\nclient_port = {1}\nmessaging_port = {2}\nname = {0}\nfsync = true\nhome = /tmp/db/arakoon/{3}/db\nip = {4}\nlog_level = info\ntlog_dir = /tmp/db/arakoon/{3}/tlogs\nlog_dir = /var/log/arakoon/{3}\n\n'
 
-        TestArakoonInstaller.expected_global = '[global]\ncluster_id = {0}\ncluster = {1}\nplugins = \n\n'
-        TestArakoonInstaller.expected_base = '[{0}]\nname = {0}\nip = {1}\nclient_port = {2}\nmessaging_port = {3}\ntlog_compression = snappy\nlog_level = info\nlog_dir = /var/log/arakoon/one\nhome = /tmp/db/arakoon/one\ntlog_dir = /tmp/db/tlogs/one\nfsync = true\n\n'
-
-        # System
         def _get_my_machine_id(_client):
             return TestArakoonInstaller.nodes[_client.ip]
 
         System.get_my_machine_id = staticmethod(_get_my_machine_id)
 
-        # Configuration
-        def _get(key):
-            if key == 'ovs.core.storage.persistent':
-                return 'arakoon'
-            c = PersistentFactory.get_client()
-            if c.exists(key):
-                return c.get(key)
-            return None
-
-        def _get_int(key):
-            return int(Configuration.get(key))
-
-        def _set(key, value):
-            c = PersistentFactory.get_client()
-            c.set(key, value)
-
-        Configuration.get = staticmethod(_get)
-        Configuration.get_int = staticmethod(_get_int)
-        Configuration.set = staticmethod(_set)
-
-        Configuration.set('ovs.ports.arakoon', [22000])
-        Configuration.set('ovs.arakoon.location', '/tmp/db')
-
     @classmethod
     def setUp(cls):
         for node in TestArakoonInstaller.nodes:
             client = SSHClient(node)
-            client.dir_delete('/tmp/db')
-            client.dir_delete('/tmp/cfg')
-            client.dir_create('/tmp/db')
-            client.dir_create('/tmp/cfg')
-
-    def _get_config_path(self, cluster):
-        return '/tmp/cfg/{0}/{0}.cfg'.format(cluster)
+            root_client = SSHClient(node, username='root')
+            root_client.dir_delete('/tmp/db')
+            root_client.dir_create('/tmp/db')
+            client.dir_delete(TestArakoonInstaller.cluster_config_path)
+            client.dir_create(TestArakoonInstaller.cluster_config_path)
 
     def test_single_node(self):
-        base_port = Configuration.get('ovs.ports.arakoon')[0]
-        cluster = 'one'
         node = sorted(TestArakoonInstaller.nodes.keys())[0]
-        ArakoonInstaller.create_cluster(cluster, node, [])
-        contents = SSHClient(node).file_read(self._get_config_path(cluster))
-        expected = TestArakoonInstaller.expected_global.format(cluster, TestArakoonInstaller.nodes[node])
-        expected += TestArakoonInstaller.expected_base.format(TestArakoonInstaller.nodes[node], node, base_port, base_port + 1)
+        result = ArakoonInstaller.create_cluster(TestArakoonInstaller.cluster_name, node, [], '/tmp/db')
+        contents = SSHClient(node).file_read(TestArakoonInstaller.cluster_config_file)
+        expected = TestArakoonInstaller.expected_global.format(TestArakoonInstaller.nodes[node], TestArakoonInstaller.cluster_name)
+        expected += TestArakoonInstaller.expected_base.format(TestArakoonInstaller.nodes[node], result['client_port'], result['messaging_port'], TestArakoonInstaller.cluster_name, node)
         self.assertEqual(contents.strip(), expected.strip())
 
     def test_multi_node(self):
-        base_port = Configuration.get('ovs.ports.arakoon')[0]
-        cluster = 'one'
         nodes = sorted(TestArakoonInstaller.nodes.keys())
         nodes = dict((node, SSHClient(node)) for node in nodes)
         first_node = nodes.keys()[0]
-        ArakoonInstaller.create_cluster(cluster, first_node, [])
-        for node in nodes[1:]:
-            ArakoonInstaller.extend_cluster(first_node, node, cluster, [])
-        expected = TestArakoonInstaller.expected_global.format(cluster, ','.join(TestArakoonInstaller.nodes[node] for node in nodes))
+        result = ArakoonInstaller.create_cluster(TestArakoonInstaller.cluster_name, first_node, [], '/tmp/db')
+        for node in nodes.keys()[1:]:
+            ArakoonInstaller.extend_cluster(first_node, node, TestArakoonInstaller.cluster_name, [], '/tmp/db')
+        expected = TestArakoonInstaller.expected_global.format(','.join(TestArakoonInstaller.nodes[node] for node in nodes), TestArakoonInstaller.cluster_name)
         for node in nodes:
-            expected += TestArakoonInstaller.expected_base.format(TestArakoonInstaller.nodes[node], node, base_port, base_port + 1)
+            expected += TestArakoonInstaller.expected_base.format(TestArakoonInstaller.nodes[node], result['client_port'], result['messaging_port'], TestArakoonInstaller.cluster_name, node)
         expected = expected.strip()
         for node, client in nodes.iteritems():
-            contents = client.file_read(self._get_config_path(cluster))
+            contents = client.file_read(TestArakoonInstaller.cluster_config_file)
             self.assertEqual(contents.strip(), expected.strip())
-        ArakoonInstaller.shrink_cluster(nodes[1], first_node, cluster)
-        expected = TestArakoonInstaller.expected_global.format(cluster, ','.join(TestArakoonInstaller.nodes[node] for node in nodes[1:]))
+        ArakoonInstaller.shrink_cluster(nodes.keys()[1], first_node, TestArakoonInstaller.cluster_name)
+        expected = TestArakoonInstaller.expected_global.format(','.join(TestArakoonInstaller.nodes[node] for node in nodes[1:]), TestArakoonInstaller.cluster_name)
         for node in nodes.keys()[1:]:
-            expected += TestArakoonInstaller.expected_base.format(TestArakoonInstaller.nodes[node], node, base_port, base_port + 1)
+            expected += TestArakoonInstaller.expected_base.format(TestArakoonInstaller.nodes[node], result['client_port'], result['messaging_port'], TestArakoonInstaller.cluster_name, node)
         expected = expected.strip()
         for node, client in nodes.iteritems():
             if node == first_node:
                 continue
-            contents = client.file_read(self._get_config_path(cluster))
+            contents = client.file_read(TestArakoonInstaller.cluster_config_file)
             self.assertEqual(contents.strip(), expected.strip())
 
 
