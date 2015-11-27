45c8a81c4a66598d60f6c04f9ad1f719f3578d63
Kenneth Henderick <kenneth@ketronic.be>
2015-11-27 16:25:02 +0100

    OVS-3621 - Memcache restart fixes

--- ovs/extensions/generic/plugins.py
+++ ovs/extensions/generic/plugins.py
@@ -28,43 +28,39 @@ class PluginManager(object):
         if ServiceManager.has_service('ovs-watcher-framework', SSHClient('127.0.0.1', username='root')):
             # If the watcher is running, 'ovs setup' was executed and we need to restart everything to load
             # the plugin. In the other case, the plugin will be loaded once 'ovs setup' is executed
+            print 'Installing plugin into Open vStorage'
             from ovs.dal.lists.storagerouterlist import StorageRouterList
-            clients = []
+            clients = {}
+            masters = StorageRouterList.get_masters()
+            slaves = StorageRouterList.get_slaves()
             try:
-                for storagerouter in StorageRouterList.get_storagerouters():
-                    clients.append(SSHClient(storagerouter, username='root'))
+                for sr in masters + slaves:
+                    clients[sr] = SSHClient(sr, username='root')
             except UnableToConnectException:
                 raise RuntimeError('Not all StorageRouters are reachable')
+            memcached = 'memcached'
+            watcher = 'watcher-framework'
+            for sr in masters + slaves:
+                if ServiceManager.has_service(watcher, clients[sr]):
+                    print '- Stopping watcher on {0} ({1})'.format(sr.name, sr.ip)
+                    ServiceManager.stop_service(watcher, clients[sr])
+            for sr in masters:
+                print '- Restarting memcached on {0} ({1})'.format(sr.name, sr.ip)
+                ServiceManager.restart_service(memcached, clients[sr])
+            for sr in masters + slaves:
+                if ServiceManager.has_service(watcher, clients[sr]):
+                    print '- Starting watcher on {0} ({1})'.format(sr.name, sr.ip)
+                    ServiceManager.start_service(watcher, clients[sr])
 
-            for client in clients:
-                for service_name in ['watcher-framework', 'memcached']:
-                    ServiceManager.stop_service(service_name, client=client)
-                    wait = 30
-                    while wait > 0:
-                        if ServiceManager.get_service_status(service_name, client=client) is False:
-                            break
-                        time.sleep(1)
-                        wait -= 1
-                    if wait == 0:
-                        raise RuntimeError('Could not stop service: {0}'.format(service_name))
-
-            for client in clients:
-                for service_name in ['memcached', 'watcher-framework']:
-                    ServiceManager.start_service(service_name, client=client)
-                    wait = 30
-                    while wait > 0:
-                        if ServiceManager.get_service_status(service_name, client=client) is True:
-                            break
-                        time.sleep(1)
-                        wait -= 1
-                    if wait == 0:
-                        raise RuntimeError('Could not start service: {0}'.format(service_name))
-
+            print '- Execute model migrations'
             from ovs.dal.helpers import Migration
             Migration.migrate()
 
             from ovs.lib.helpers.toolbox import Toolbox
             ip = System.get_my_storagerouter().ip
             functions = Toolbox.fetch_hooks('plugin', 'postinstall')
+            if len(functions) > 0:
+                print '- Execute post installation scripts'
             for function in functions:
                 function(ip=ip)
+            print 'Installing plugin into Open vStorage: Completed'
