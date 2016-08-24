312b7012951a778bd0424fea225578a2bd9c6b96
Jeffrey Devloo <jeffreydevloo@hotmail.com>
2016-08-24 14:35:57 +0200

    Correctly closing stfp connections Now reading channel output based on its buffer size. Previously the stdout reading could hang when the output exceeded its buffer size

--- ovs/extensions/generic/sshclient.py
+++ ovs/extensions/generic/sshclient.py
@@ -54,7 +54,7 @@ def connected():
             :param self
             """
             try:
-                if self.client is not None and not self.client.is_connected():
+                if self._client is not None and not self._client.is_connected():
                     self._connect()
                 return outer_function(self, *args, **kwargs)
             except AttributeError as ex:
@@ -112,7 +112,7 @@ class SSHClient(object):
             raise ValueError('The endpoint parameter should be either an ip address or a StorageRouter')
 
         self.ip = ip
-        self.client = None
+        self._client = None
         self.local_ips = [lip.strip() for lip in check_output("ip a | grep 'inet ' | sed 's/\s\s*/ /g' | cut -d ' ' -f 3 | cut -d '/' -f 1", shell=True).strip().splitlines()]
         self.is_local = self.ip in self.local_ips
         self.password = password
@@ -146,7 +146,7 @@ class SSHClient(object):
                 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                 client.is_connected = types.MethodType(is_connected, client)
                 SSHClient.client_cache[key] = client
-            self.client = SSHClient.client_cache[key]
+            self._client = SSHClient.client_cache[key]
         self._connect()
 
     def __del__(self):
@@ -168,10 +168,10 @@ class SSHClient(object):
 
         try:
             try:
-                self.client.connect(self.ip, username=self.username, password=self.password)
+                self._client.connect(self.ip, username=self.username, password=self.password)
             except:
                 try:
-                    self.client.close()
+                    self._client.close()
                 except:
                     pass
                 raise
@@ -189,7 +189,7 @@ class SSHClient(object):
         if self.is_local is True:
             return
 
-        self.client.close()
+        self._client.close()
 
     @staticmethod
     def shell_safe(path_to_check):
@@ -200,12 +200,13 @@ class SSHClient(object):
         return "".join([("\\" + _) if _ in " '\";`|" else _ for _ in path_to_check])
 
     @connected()
-    def run(self, command, debug=False, suppress_logging=False):
+    def run(self, command, debug=False, suppress_logging=False, timeout=0.0):
         """
         Executes a shell command
         :param suppress_logging: Do not log anything
         :param command: Command to execute
         :param debug: Extended logging and stderr output returned
+        :param timeout: will wait x seconds before timing out on reading the output (0.0 means no timeout)
         """
         if self.is_local is True:
             stderr = None
@@ -238,18 +239,45 @@ class SSHClient(object):
         else:
             if isinstance(command, list):
                 command = ' '.join(command)
-            _, stdout, stderr = self.client.exec_command(command)  # stdin, stdout, stderr
+            stdin, stdout, stderr = self._client.exec_command(command)  # stdin, stdout, stderr
+            stdin.close()  # we do not need stdin.
+
+            channel = stdout.channel
+            # indicate that we're not going to write to that channel anymore
+            channel.shutdown_write()
+            # read stdout/stderr in order to prevent read block hangs
+            stdout_chunks = []
+            # chunked read to prevent stalls
+            while not channel.closed:  # stop if channel was closed prematurely
+                read_list, _, _ = select.select([stdout.channel], [], [], timeout)
+                for content in read_list:
+                    if content.recv_ready():
+                        stdout_chunks.append(stdout.channel.recv(len(content.in_buffer)))
+                    if content.recv_stderr_ready():
+                        # make sure to read stderr to prevent stall
+                        stderr.channel.recv_stderr(len(content.in_stderr_buffer))
+                if stdout.channel.exit_status_ready() and not stderr.channel.recv_stderr_ready() \
+                        and not stdout.channel.recv_ready():
+                    # indicate that we're not going to read from this channel anymore
+                    stdout.channel.shutdown_read()
+                    # close the channel
+                    stdout.channel.close()
+                    break  # exit as remote side is finished and our buffers are empty
             exit_code = stdout.channel.recv_exit_status()
             if exit_code != 0:  # Raise same error as check_output
-                stderr = ''.join(stderr.readlines()).replace(u'\u2018', u'"').replace(u'\u2019', u'"')
-                stdout = ''.join(stdout.readlines()).replace(u'\u2018', u'"').replace(u'\u2019', u'"')
+                error_output = ''.join(stderr.readlines()).replace(u'\u2018', u'"').replace(u'\u2019', u'"')
+                exec_output = ''.join(stdout.readlines()).replace(u'\u2018', u'"').replace(u'\u2019', u'"')
                 if suppress_logging is False:
-                    SSHClient._logger.error('Command "{0}" failed with output "{1}" and error "{2}"'.format(command, stdout, stderr))
-                raise CalledProcessError(exit_code, command, stdout)
+                    SSHClient._logger.error('Command "{0}" failed with output "{1}" and error "{2}"'
+                                            .format(command, exec_output, error_output))
+                raise CalledProcessError(exit_code, command, exec_output)
+            # close all the pseudofiles
+            stdout.close()
+            stderr.close()
             if debug:
-                return '\n'.join(line.rstrip() for line in stdout).strip(), stderr
+                return ''.join(stdout_chunks).strip(), stderr
             else:
-                return '\n'.join(line.rstrip() for line in stdout).strip()
+                return ''.join(stdout_chunks).strip()
 
     def dir_create(self, directories):
         """
@@ -497,11 +525,11 @@ if os.path.islink('{0}'):
                 the_file.write(contents)
             os.close(handle)
             try:
-                sftp = self.client.open_sftp()
+                sftp = self._client.open_sftp()
                 sftp.put(temp_filename, filename)
-                sftp.close()
             finally:
                 os.remove(temp_filename)
+                sftp.close()
 
     @connected()
     def file_upload(self, remote_filename, local_filename):
@@ -513,9 +541,11 @@ if os.path.islink('{0}'):
         if self.is_local is True:
             check_output('cp -f "{0}" "{1}"'.format(local_filename, remote_filename), shell=True)
         else:
-            sftp = self.client.open_sftp()
-            sftp.put(local_filename, remote_filename)
-            sftp.close()
+            try:
+                sftp = self._client.open_sftp()
+                sftp.put(local_filename, remote_filename)
+            finally:
+                sftp.close()
 
     def file_exists(self, filename):
         """
