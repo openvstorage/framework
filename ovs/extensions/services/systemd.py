e4135b5da9f509457e7aa9b0cc0f22358e170e3c
kvanhijf <kevin.vanhijfte@openvstorage.com>
2016-10-27 17:33:31 +0200

    #1087 - Proper service removal for Ubuntu 16.04

--- ovs/extensions/services/systemd.py
+++ ovs/extensions/services/systemd.py
@@ -136,7 +136,10 @@ class Systemd(object):
         :return: None
         """
         name = Systemd._get_name(name, client)
-        client.run('systemctl disable {0}.service')
+        try:
+            client.run('systemctl disable {0}.service')
+        except CalledProcessError:
+            pass  # Service already disabled
         client.file_delete('/lib/systemd/system/{0}.service'.format(name))
         client.run('systemctl daemon-reload')
 
