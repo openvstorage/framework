43f438585682ad07a2d25f029da5b17f5ebf18cc
Kevin Van Hijfte <kevin.vanhijfte@openvstorage.com>
2017-01-18 09:07:26 +0100

    1339 - Review feedback

--- ovs/lib/storagerouter.py
+++ ovs/lib/storagerouter.py
@@ -919,7 +919,7 @@ class StorageRouterController(object):
                 if sr in storage_routers_offline:
                     raise Exception('Storage Router "{0}" passed as "offline Storage Router" appears to be reachable'.format(sr.name))
                 if sr == storage_router:
-                    mtpt_pids = temp_client.run('lsof -t +D /mnt/{0} || true'.format(vpool.name), allow_insecure=True).splitlines()
+                    mtpt_pids = temp_client.run("lsof -t +D '/mnt/{0}' || true".format(vpool.name.replace(r"'", r"'\''")), allow_insecure=True).splitlines()
                     if len(mtpt_pids) > 0:
                         raise RuntimeError('vPool cannot be deleted. Following processes keep the vPool mountpoint occupied: {0}'.format(', '.join(mtpt_pids)))
                 with remote(temp_client.ip, [LocalStorageRouterClient]) as rem:
