b3e5be82460008a4b12d7b3850e572204830a3eb
Kenneth Henderick <kenneth@ketronic.be>
2017-01-24 16:11:21 +0100

    Remove SSH keygen from post installation script

--- scripts/install/openvstorage-core.postinst.py
+++ scripts/install/openvstorage-core.postinst.py
@@ -88,32 +88,3 @@ if __name__ == '__main__':
             contents += '\novs soft core unlimited'
         file_write(limits_file, '{0}\n'.format(contents))
 
-    # Generate SSH keys
-    root_ssh_folder = '{0}/.ssh'.format(check_output('echo ~root', shell=True).strip())
-    ovs_ssh_folder = '{0}/.ssh'.format(check_output('echo ~ovs', shell=True).strip())
-    private_key_filename = '{0}/id_rsa'
-    authorized_keys_filename = '{0}/authorized_keys'
-    known_hosts_filename = '{0}/known_hosts'
-
-    check_output('su - ovs -c "mkdir -p {0}"'.format(ovs_ssh_folder), shell=True)
-    check_output('su - ovs -c "chmod 755 {0}"'.format(ovs_ssh_folder), shell=True)
-
-    # Generate keys for root
-    if not os.path.exists(private_key_filename.format(root_ssh_folder)):
-        check_output("ssh-keygen -t rsa -b 4096 -f {0} -N ''".format(private_key_filename.format(root_ssh_folder)), shell=True)
-    # Generate keys for ovs
-    if not os.path.exists(private_key_filename.format(ovs_ssh_folder)):
-        check_output('su - ovs -c "ssh-keygen -t rsa -b 4096 -f {0} -N \'\'"'.format(private_key_filename.format(ovs_ssh_folder)), shell=True)
-
-    root_authorized_keys = authorized_keys_filename.format(root_ssh_folder)
-    ovs_authorized_keys = authorized_keys_filename.format(ovs_ssh_folder)
-    root_known_hosts = known_hosts_filename.format(root_ssh_folder)
-    ovs_known_hosts = known_hosts_filename.format(ovs_ssh_folder)
-
-    for filename in [root_authorized_keys, root_known_hosts]:
-        check_output('touch {0}'.format(filename), shell=True)
-        check_output('chmod 600 {0}'.format(filename), shell=True)
-
-    for filename in [ovs_authorized_keys, ovs_known_hosts]:
-        check_output('su - ovs -c "touch {0}"'.format(filename), shell=True)
-        check_output('su - ovs -c "chmod 600 {0}"'.format(filename), shell=True)
