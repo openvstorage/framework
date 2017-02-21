cbc7b25108c56fd7c5763123d89447b8c7a258d2
Kenneth Henderick <kenneth@ketronic.be>
2017-02-21 17:21:25 +0100

    Allow a resettable waiter

--- ovs/extensions/generic/threadhelpers.py
+++ ovs/extensions/generic/threadhelpers.py
@@ -20,16 +20,32 @@ from threading import Lock
 
 class Waiter(object):
 
-    def __init__(self, target):
+    def __init__(self, target, auto_reset=False):
         self._target = target
         self._counter = 0
         self._lock = Lock()
+        self._auto_release_lock = Lock()
+        self._auto_reset = auto_reset
+        self._auto_release_counter = 0
 
     def wait(self, timeout=5):
         with self._lock:
             self._counter += 1
-        start = time.time()
-        while self._counter < self._target:
-            time.sleep(0.05)
-            if time.time() - start > timeout:
-                raise RuntimeError('Not all peers were available within {0}s'.format(timeout))
+            reached = self._counter == self._target
+            if reached is True and self._auto_reset is True:
+                while self._auto_release_counter < self._target - 1:
+                    time.sleep(0.05)
+                self._counter = 0
+                with self._auto_release_lock:
+                    self._auto_release_lock = 0
+        if reached is False:
+            start = time.time()
+            while self._counter < self._target:
+                time.sleep(0.05)
+                if time.time() - start > timeout:
+                    raise RuntimeError('Not all peers were available within {0}s'.format(timeout))
+            with self._auto_release_lock:
+                self._auto_release_counter += 1
+
+    def get_counter(self):
+        return self._counter
