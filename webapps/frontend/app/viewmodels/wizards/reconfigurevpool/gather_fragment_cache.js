3474474c481ad6ccf3272888e916421e5588549e
Jeffrey Devloo <jeffreydevloo@hotmail.com>
2017-09-20 13:18:32 +0200

    Return distinct backends

--- webapps/frontend/app/viewmodels/wizards/reconfigurevpool/gather_fragment_cache.js
+++ webapps/frontend/app/viewmodels/wizards/reconfigurevpool/gather_fragment_cache.js
@@ -35,69 +35,6 @@ define([
         self.fragmentCacheSettings      = ko.observableArray(['write', 'read', 'rw', 'none']);
         self.preset                     = ko.observable();
 
-        // Computed
-        self.canContinue = ko.computed(function() {
-            var reasons = [], fields = [];
-            return { value: reasons.length === 0, reasons: reasons, fields: fields };
-        });
-        self.fragmentCacheBackend = ko.computed({
-            read: function() {
-                var backendInfo = self.data.cachingData.fragmentCache.backendInfo;
-                return self.data.getBackend(backendInfo.backend_guid());
-            },
-            write: function(backend) {
-                // Mutate the backend info
-                var backendInfo = self.data.cachingData.fragmentCache.backendInfo;
-                backendInfo.name(backend.name);
-                backendInfo.backend_guid(backend.backend_guid);
-                backendInfo.alba_backend_guid(backend.guid);
-            }
-        });
-        self.fragmentCacheBackends = ko.computed(function() {
-            var backendInfo = self.data.cachingData.fragmentCache.backendInfo;
-            var connectionInfo = backendInfo.connectionInfo;
-            var locationKey = self.data.buildLocationKey(connectionInfo);
-            self.data.backendsFilter(locationKey);
-            return self.data.filteredBackends();
-        });
-        self.localBackendsAvailable = ko.computed(function() {
-            var connectionInfo = self.data.cachingData.fragmentCache.backendInfo.connectionInfo;
-            var useLocalBackend = !!ko.utils.unwrapObservable(connectionInfo.local);
-            var localBackendsRequiredAmount = useLocalBackend === true ? 2 : 1;
-            return Object.keys(self.data.filterBackendsByLocationKey('local')).length >= localBackendsRequiredAmount;
-        });
-        self.enhancedPresets = ko.computed(function() {
-            return self.data.parsePresets(self.fragmentCacheBackend())
-        });
-        self.preset = ko.computed({
-            read: function() {
-                var parsedPreset = undefined;
-                if (self.fragmentCacheBackend() === undefined) {
-                    return parsedPreset
-                }
-                var backendInfo = self.data.cachingData.fragmentCache.backendInfo;
-                if (ko.utils.unwrapObservable(backendInfo.preset) === undefined) {
-                    return parsedPreset;
-                }
-                var preset = self.data.getPreset(backendInfo.alba_backend_guid(), backendInfo.preset());
-                if (preset === undefined) {
-                    // No preset could be found for our current setting. Attempt to reconfigure it
-                    var enhancedPresets = self.enhancedPresets();
-                    if (enhancedPresets.length > 0) {
-                        parsedPreset = enhancedPresets[0];
-                        backendInfo.preset(enhancedPresets[0].name);  // This will trigger this compute to trigger again but also correct the mistake
-                    }
-                    return parsedPreset
-                }
-                return self.data.parsePreset(preset);
-            },
-            write: function(preset) {
-                console.log(preset);
-                var backendInfo = self.data.cachingData.fragmentCache.backendInfo;
-                backendInfo.preset(preset.name);
-            }
-        });
-
         // Functions
         self.loadBackends = function() {
             var connectionInfo = self.data.cachingData.fragmentCache.backendInfo.connectionInfo;
@@ -117,18 +54,91 @@ define([
             backendInfo.policies([]);
         };
 
+        // Computed
+        self.canContinue = ko.computed(function() {
+            var reasons = [], fields = [];
+            return { value: reasons.length === 0, reasons: reasons, fields: fields };
+        });
+        self.fragmentCacheBackend = ko.computed({
+            read: function() {
+                var backendInfo = self.data.cachingData.fragmentCache.backendInfo;
+                var backend = self.data.getBackend(backendInfo.backend_guid());
+                if (backend === undefined) {
+                    // Return the first of the list
+                    var backends = ko.utils.unwrapObservable(self.fragmentCacheBackends);
+                    if (backends !== undefined && backends.length > 0) {
+                        backend = backends[0];
+                        self.fragmentCacheBackend(backend)
+                    } else {
+                        self.resetBackend()  // No backend to be set, update our model
+                    }
+                }
+                return backend;
+            },
+            write: function(backend) {
+                // Mutate the backend info
+                var backendInfo = self.data.cachingData.fragmentCache.backendInfo;
+                backendInfo.name(backend.name);
+                backendInfo.backend_guid(backend.backend_guid);
+                backendInfo.alba_backend_guid(backend.guid);
+            }
+        });
+        self.fragmentCacheBackends = ko.computed(function() {
+            var backendInfo = self.data.cachingData.fragmentCache.backendInfo;
+            var connectionInfo = backendInfo.connectionInfo;
+            var locationKey = self.data.buildLocationKey(connectionInfo);
+            self.data.backendsFilter(locationKey);
+            var backends = self.data.filteredBackends();
+            if (backends.length === 0) {
+                // Reset our backend
+                self.resetBackend()
+            }
+            return backends;
+        });
+        self.localBackendsAvailable = ko.computed(function() {
+            var connectionInfo = self.data.cachingData.fragmentCache.backendInfo.connectionInfo;
+            var useLocalBackend = !!ko.utils.unwrapObservable(connectionInfo.local);
+            var localBackendsRequiredAmount = useLocalBackend === true ? 2 : 1;
+            var backends = self.data.filterBackendsByLocationKey('local');
+            return self.data.filterBackendsByLocationKey('local').length >= localBackendsRequiredAmount;
+        });
+        self.enhancedPresets = ko.computed(function() {
+            return self.data.parsePresets(self.fragmentCacheBackend())
+        });
+        self.preset = ko.computed({
+            read: function() {
+                var parsedPreset = undefined;
+                if (self.fragmentCacheBackend() === undefined) {
+                    return parsedPreset
+                }
+                var backendInfo = self.data.cachingData.fragmentCache.backendInfo;
+                var preset = self.data.getPreset(backendInfo.alba_backend_guid(), backendInfo.preset());
+                if (preset === undefined) {
+                    // No preset could be found for our current setting. Attempt to reconfigure it
+                    var enhancedPresets = self.enhancedPresets();
+                    if (enhancedPresets.length > 0) {
+                        parsedPreset = enhancedPresets[0];
+                        self.preset(parsedPreset);  // This will trigger this compute to trigger again but also correct the mistake
+                    }
+                    return parsedPreset
+                }
+                return self.data.parsePreset(preset);
+            },
+            write: function(preset) {
+                console.log(preset);
+                var backendInfo = self.data.cachingData.fragmentCache.backendInfo;
+                backendInfo.preset(preset.name);
+            }
+        });
+
         // Durandal
         self.activate = function() {
             if (self.actived === false) {
                 self.data.loadBackends();
                 var connectionInfo = self.data.cachingData.fragmentCache.backendInfo.connectionInfo;
-                if (!!ko.utils.unwrapObservable(connectionInfo.local) === false && !['', undefined, null].contains(connectionInfo().host())) {
+                if (!!ko.utils.unwrapObservable(connectionInfo.local) === false && !['', undefined, null].contains(connectionInfo.host())) {
                     self.data.loadBackends(connectionInfo);
                 }
-                self.subscriptions.push(ko.watch(connectionInfo, {depth: 1}, function(parents, child, item) {
-                    // Reset our information when any change is detected in theconnection information
-                    self.resetBackend()
-                }));
                 self.actived = true;
             }
         };
