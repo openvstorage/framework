dac2b27bdf811c0821da19891ef9e465a4929d86
Jeffrey Devloo <jeffreydevloo@hotmail.com>
2017-11-07 17:52:21 +0100

    Initial changes and docs

--- ovs/extensions/support/agent.py
+++ ovs/extensions/support/agent.py
@@ -50,6 +50,8 @@ class SupportAgent(object):
         self._package_manager = PackageFactory.get_manager()
         self._service_manager = ServiceFactory.get_manager()
         self._client = SSHClient(endpoint=self._storagerouter)
+        self._node_id = self._storagerouter.machine_id.replace(r"'", r"'\''")
+        self._openvpn_service_name = 'openvpn@ovs_{0}-{1}'.format(self._cluster_id, self._node_id)
 
         self.interval = Configuration.get('/ovs/framework/support|interval', default=60)
 
@@ -104,37 +106,12 @@ class SupportAgent(object):
         """
         try:
             SupportAgent._logger.debug('Processing: {0}'.format(task_code))
-            node_id = self._storagerouter.machine_id.replace(r"'", r"'\''")
-            service_name = 'openvpn@ovs_{0}-{1}'.format(self._cluster_id, node_id)
-
             if task_code == 'OPEN_TUNNEL':
-                if self._service_type == 'upstart':
-                    check_output('service openvpn stop', shell=True)
-                else:
-                    check_output("systemctl stop '{0}' || true".format(service_name), shell=True)
-                check_output('rm -f /etc/openvpn/ovs_*', shell=True)
-                for filename, contents in metadata['files'].iteritems():
-                    with open(filename, 'w') as the_file:
-                        the_file.write(base64.b64decode(contents))
-                if self._service_type == 'upstart':
-                    check_output('service openvpn start', shell=True)
-                else:
-                    check_output("systemctl start '{0}'".format(service_name), shell=True)
+                self.open_tunnel(metadata)
             elif task_code == 'CLOSE_TUNNEL':
-                if self._service_type == 'upstart':
-                    check_output('service openvpn stop', shell=True)
-                else:
-                    check_output("systemctl stop '{0}'".format(service_name), shell=True)
-                check_output('rm -f /etc/openvpn/ovs_*', shell=True)
+                self.close_tunnel(metadata)
             elif task_code == 'UPLOAD_LOGFILES':
-                logfile = check_output('ovs collect logs', shell=True).strip()
-                check_output("mv '{0}' '/tmp/{1}'; curl -T '/tmp/{1}' 'ftp://{2}' --user '{3}:{4}'; rm -f '{0}' '/tmp/{1}'".format(
-                    logfile.replace(r"'", r"'\''"),
-                    metadata['filename'].replace(r"'", r"'\''"),
-                    metadata['endpoint'].replace(r"'", r"'\''"),
-                    metadata['user'].replace(r"'", r"'\''"),
-                    metadata['password'].replace(r"'", r"'\''")
-                ), shell=True)
+                self.upload_files(metadata)
             else:
                 raise RuntimeError('Unknown task')
         except Exception:
@@ -143,18 +120,93 @@ class SupportAgent(object):
         finally:
             SupportAgent._logger.debug('Completed')
 
+    def open_tunnel(self, metadata):
+        """
+        Opens up the openvpn tunnel
+        :param metadata: Metadata about the files to use for tunnel
+        :type metadata: dict
+        Example: {'files': {...}}.
+        The files dict will contain file names (keys) and their contents (value) Example: {my_file: my_file_contents}
+        These files will always be /etc/openvpn/ovs_ + 'ca.crt', 'ta.key', '{0}.crt'.format(identifier), '{0}.key'.format(identifier), '{0}.conf'.format(identifier)
+        with identifier = cluster id + node id
+        :return: None
+        :rtype: NoneType
+        """
+        # Close tunnel and update configs
+        self.close_tunnel(metadata)
+        for filename, contents in metadata['files'].iteritems():
+            with open(filename, 'w') as the_file:
+                the_file.write(base64.b64decode(contents))
+        if self._service_type == 'upstart':
+            check_output('service openvpn start', shell=True)
+        else:
+            check_output("systemctl start '{0}'".format(self._openvpn_service_name), shell=True)
+
+    @staticmethod
+    def upload_files(metadata):
+        """
+        Upload the collected log files to a destination
+        :param metadata: Metadata about the destination.
+        Example: {'filename': name to upload the tarball as, 'endpoint': ip of the ftp, 'user': an ftp user, 'password': ftp users password}
+        :type metadata: dict
+        :return: None
+        :rtype: NoneType
+        """
+        logfile = check_output('ovs collect logs', shell=True).strip()
+        check_output("mv '{0}' '/tmp/{1}'; curl -T '/tmp/{1}' 'ftp://{2}' --user '{3}:{4}'; rm -f '{0}' '/tmp/{1}'".format(
+            logfile.replace(r"'", r"'\''"),
+            metadata['filename'].replace(r"'", r"'\''"),
+            metadata['endpoint'].replace(r"'", r"'\''"),
+            metadata['user'].replace(r"'", r"'\''"),
+            metadata['password'].replace(r"'", r"'\''")), shell=True)
+
+    def close_tunnel(self, metadata):
+        """
+        Closes the openvpn tunnel
+        @TODO: only close the ovs remote tunnel on 14.04 (all tunnels are currently closed)
+        :param metadata: Metadata for the task (empty dict for close tunnel)
+        :type metadata: dict
+        :return: None
+        :rtype: NoneType
+        """
+        _ = metadata
+        if self._service_type == 'upstart':
+            check_output('service openvpn stop', shell=True)
+        else:
+            check_output("systemctl stop '{0}'".format(self._openvpn_service_name), shell=True)
+        check_output('rm -f /etc/openvpn/ovs_*', shell=True)
+
+    def _send_heartbeat(self):
+        """
+        Send heart beat to the monitoring server
+        :raises RuntimeError when in valid status code is returned by the api
+        :return: Returns the response from the server
+        Example return: {u'tasks': [{'code': 'OPEN_TUNNEL', 'metadata': {'files': {'/etc/openvpn/ovs_ca.crt': 'CERTIFACTECONTENTS'}}}]}
+        The tasks returned from the server are classified by a task code which is one of the following: OPEN_TUNNEL, CLOSE_TUNNEL, UPLOAD_LOGFILES
+        - OPEN TUNNEL tasks receive metadata which has a files entry. Example: {'metadata': {'files': {...}}}.
+          The files dict will contain file names (keys) and their contents (value) Example: {my_file: my_file_contents}
+          These files will always be /etc/openvpn/ovs_ + 'ca.crt', 'ta.key', '{0}.crt'.format(identifier), '{0}.key'.format(identifier), '{0}.conf'.format(identifier)
+          with identifier = cluster id + node id
+        - CLOSE TUNNEL tasks have no metadata
+        - UPLOAD_LOGFILES tasks receive metadata about where to upload tar balled logs
+          Example: {'metadata': {'filename': name to upload the tarball as, 'endpoint': ip of the ftp, 'user': an ftp user, 'password': ftp users password}}
+        :rtype: dict
+        """
+        response = requests.post(url='https://monitoring.openvstorage.com/api/support/heartbeat/',
+                                 data={'data': json.dumps(self.get_heartbeat_data())},
+                                 headers={'Accept': 'application/json; version=1'})
+        if response.status_code != 200:
+            raise RuntimeError('Received invalid status code: {0} - {1}'.format(response.status_code, response.text))
+        return response.json()
+
     def run(self):
         """
         Executes a call
         """
-        SupportAgent._logger.debug('Processing heartbeat')
+        self._logger.debug('Processing heartbeat')
         try:
-            response = requests.post(url='https://monitoring.openvstorage.com/api/support/heartbeat/',
-                                     data={'data': json.dumps(self.get_heartbeat_data())},
-                                     headers={'Accept': 'application/json; version=1'})
-            if response.status_code != 200:
-                raise RuntimeError('Received invalid status code: {0} - {1}'.format(response.status_code, response.text))
-            return_data = response.json()
+            return_data = self._send_heartbeat()
+            self._logger.debug('Requested return data: {0}'.format(return_data))
         except Exception:
             SupportAgent._logger.exception('Unexpected error during support call')
             raise
@@ -164,7 +216,7 @@ class SupportAgent(object):
             self._storagerouter.last_heartbeat = time.time()
             self._storagerouter.save()
         except Exception:
-            SupportAgent._logger.exception('Could not save last heartbeat timestamp')
+            self._logger.exception('Could not save last heartbeat timestamp')
             # Ignore this error, it's not mandatory for the support agent
 
         if Configuration.get('/ovs/framework/support|remote_access') is True:
@@ -172,9 +224,10 @@ class SupportAgent(object):
                 for task in return_data['tasks']:
                     self._process_task(task['code'], task['metadata'])
             except Exception:
-                SupportAgent._logger.exception('Unexpected error processing tasks')
+                self._logger.exception('Unexpected error processing tasks')
                 raise
 
+        # Currently not returned by the monitoring server
         if 'interval' in return_data:
             interval = return_data['interval']
             if interval != self.interval:
