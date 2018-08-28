b8fcec2bd38075c640d9e19d36a9540efca5e319
Simon Van den Bossche <simon.vandenbossche@openvstorage.com>
2018-08-28 14:02:24 +0200

    Framework sync_with_reality now can cope with S3 disks

--- ovs/lib/disk.py
+++ ovs/lib/disk.py
@@ -17,9 +17,11 @@
 """
 DiskController module
 """
+from ovs.constants.s3 import S3_BASE
 from ovs.dal.hybrids.disk import Disk
 from ovs.dal.hybrids.diskpartition import DiskPartition
 from ovs.dal.hybrids.storagerouter import StorageRouter
+from ovs.extensions.generic.configuration import Configuration
 from ovs_extensions.generic.disk import DiskTools, Disk as GenericDisk, Partition as GenericPartition
 from ovs.extensions.generic.logger import Logger
 from ovs.extensions.generic.sshclient import SSHClient, UnableToConnectException
@@ -49,7 +51,8 @@ class DiskController(object):
             DiskController._logger.exception('Could not connect to StorageRouter {0}'.format(storagerouter.ip))
             raise
 
-        disks, name_alias_mapping = DiskTools.model_devices(client)
+        s3 = Configuration.get(S3_BASE, default=False)
+        disks, name_alias_mapping = DiskTools.model_devices(client, s3=s3)
         disks_by_name = dict((disk.name, disk) for disk in disks)
         alias_name_mapping = name_alias_mapping.reverse_mapping()
         # Sync the model
