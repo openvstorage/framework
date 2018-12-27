c9e3cc30e377c7a558c555f0588caa1d8666a517
Simon Van den Bossche <simon.vandenbossche@openvstorage.com>
2018-12-27 12:10:58 +0100

    Now can run scripts, without arguments though
    TODO:
    -allow arguments to be given to scripts
    -change entry.py to ovs

--- ovs/cli/__init__.py
+++ ovs/cli/__init__.py
@@ -10,6 +10,7 @@ from .remove import remove_group
 from .monitor import monitor_group
 from .services import start_group, stop_group
 from ovs_extensions.cli.cli import unittest
+from ovs_extensions.constants.scripts import BASE_SCRIPTS
 from IPython import embed
 
 
@@ -20,28 +21,31 @@ However when loading in all other commands, the imports might/do fetch instances
 Which don't do anything or cannot be instantiated
 Thus we have to import controllers whenever we invoke a command :(
 """
+import subprocess
+
 
 @click.group(name='ovs', help='Open the OVS python shell', invoke_without_command=True)
 @click.pass_context
 def ovs(ctx):
     if ctx.invoked_subcommand is None:
         embed()
-    base_path = '/opt/OpenvStorage/scripts/'
-    print os.listdir(base_path)
-    print ctx.invoked_subcommand
-    if ctx.invoked_subcommand in os.listdir(base_path):
-        file = os.path.join(base_path, ctx.invoked_subcommand, 'scripts')
-        execfile(file)
+    if ctx.invoked_subcommand in [i.rstrip('.sh') for i in os.listdir(BASE_SCRIPTS)]:
+        file = os.path.join(BASE_SCRIPTS, '{0}.sh'.format(ctx.invoked_subcommand))
+        print subprocess.check_output([file])
 
     # else:
-        # Do nothing: invoke subcommand
+    # Do nothing: invoke subcommand
 
 
 groups = [setup_group, config_group, rollback, update, remove_group, monitor_group, unittest, start_group, stop_group, collect_logs]
 for group in groups:
     ovs.add_command(group)
 
- # Allow for plugins to be run as command
-for extra_group in os.listdir('/opt/OpenvStorage/scripts/'):
-    command = click.Command(name=extra_group)
+# Allow for external scripts to be run as command
+for extra_group in [file for file in os.listdir(BASE_SCRIPTS) if os.path.isfile(os.path.join(BASE_SCRIPTS, file))]:
+    script_path = os.path.join(BASE_SCRIPTS, extra_group)
+    script_name = extra_group.strip('.sh')
+    command = click.Command(name=script_name)
+    command.allow_extra_args = True
+    ignore_unknown_options=True
     ovs.add_command(command)
