0cccac6e5f912bc4816735339c692abb7f2a5038
Simon Van den Bossche <simon.vandenbossche@openvstorage.com>
2019-01-11 09:41:04 +0100

    added rename_config_files to migration code with logging

--- ovs/extensions/migration/migrator.py
+++ ovs/extensions/migration/migrator.py
@@ -32,12 +32,30 @@ class Migrator(object):
         pass
 
     @staticmethod
-    def migrate(master_ips=None, extra_ips=None):
+    def rename_config_files(logger):
+        try:
+            logger.info('elleuuuh')  #todo remove me
+            from ovs.constants.albanode import ASD_CONFIG, ASD_CONFIG_DIR
+            from ovs_extensions.constants.arakoon import ARAKOON_BASE, ARAKOON_CONFIG
+
+            for name in Configuration.list(ARAKOON_BASE):
+                whole_path = os.path.join(ARAKOON_BASE, name, 'config')
+                print whole_path
+                Configuration.rename(whole_path, ARAKOON_CONFIG.format(name))
+            for asd in Configuration.list(ASD_CONFIG_DIR):
+                Configuration.rename(ASD_CONFIG.format(asd), ASD_CONFIG.format(asd))
+        except:
+
+            pass
+
+    @staticmethod
+    def migrate(master_ips=None, extra_ips=None, logger=None):  #todo remove me logger
         """
         Executes all migrations. It keeps track of an internal "migration version" which is always increasing by one
         :param master_ips: IP addresses of the MASTER nodes
         :param extra_ips: IP addresses of the EXTRA nodes
         """
+        Migrator.rename_config_files(logger)  #todo remove me logger
         machine_id = System.get_my_machine_id()
         key = '/ovs/framework/hosts/{0}/versions'.format(machine_id)
         data = Configuration.get(key) if Configuration.exists(key) else {}
@@ -56,3 +74,5 @@ class Migrator(object):
             if end_version > start_version:
                 data[identifier] = method(start_version, master_ips, extra_ips)
         Configuration.set(key, data)
+
+
