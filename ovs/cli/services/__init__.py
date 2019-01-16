6847d8fed4e9873373e3958b3c8e80aca78587f5
Simon Van den Bossche <simon.vandenbossche@openvstorage.com>
2019-01-16 15:23:43 +0100

    Moved core function of stop and start as requested by peer review

--- ovs/cli/services/__init__.py
+++ ovs/cli/services/__init__.py
@@ -14,8 +14,8 @@
 # Open vStorage is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY of any kind.
 
+import re
 import click
-from .services import framework_start as _framework_start, framework_stop as _framework_stop
 from ovs_extensions.cli import OVSCommand
 
 extra_options = {'command_parameter_help': 'framework [ip|all]',
@@ -24,15 +24,68 @@ extra_options = {'command_parameter_help': 'framework [ip|all]',
 
 @click.command('stop', help='(Re)start Open vStorage Framework services on this node, on all nodes, or on the given ip',
                section_header='Services', **extra_options)
-@click.argument('framework', required=True, type=click.STRING)
+@click.argument('component', required=True, type=click.STRING)
 @click.argument('host', required=False, default=None, type=click.STRING)
-def framework_stop(framework, host):
-    _framework_stop(host)
+def framework_stop(component, host):
+    """
+    Stops all framework services on this node, all nodes or on specified host
+    :param host: None, 'all' or ip
+    :return:
+    """
+    from ovs.extensions.generic.sshclient import SSHClient, UnableToConnectException
+    from ovs.extensions.services.servicefactory import ServiceFactory
 
+    storagerouter_list = _get_storagerouter_list(host)
+    print 'Stopping...'
+    for storagerouter in storagerouter_list:
+        try:
+            client = SSHClient(storagerouter, username='root')
+            ServiceFactory.get_manager().stop_service('watcher-framework', client)
+        except UnableToConnectException:
+            print '{0} on {1}... failed (Node unreachable)'.format('Stopping', storagerouter.name)
+            continue
+    print 'Done'
 
 @click.command('start', help='Stop Open vStorage Framework services on this node, on all nodes, or on the given ip',
                section_header='Services', **extra_options)
-@click.argument('framework', required=True, type=click.STRING)
+@click.argument('component', required=True, type=click.STRING)
 @click.argument('host', required=False, default=None, type=click.STRING)
-def framework_start(framework, host):
-    _framework_start(host)
+def framework_start(component, host):
+    """
+    Starts all framework services on this node, all nodes or on specified host
+    :param host: None, all or ip
+    :return:
+    """
+    from ovs.extensions.generic.sshclient import SSHClient, UnableToConnectException
+    from ovs.extensions.services.servicefactory import ServiceFactory
+
+    storagerouter_list = _get_storagerouter_list(host)
+    print 'Starting...'
+    for storagerouter in storagerouter_list:
+        try:
+            client = SSHClient(storagerouter, username='root')
+            ServiceFactory.get_manager().start_service('watcher-framework', client)
+        except UnableToConnectException:
+            print '{0} on {1}... failed (Node unreachable)'.format('Starting', storagerouter.name)
+            continue
+
+    print 'Done'
+
+def _get_storagerouter_list(host):
+    from ovs.dal.lists.storagerouterlist import StorageRouterList
+    from ovs.lib.helpers.toolbox import Toolbox
+    from ovs.extensions.generic.system import System
+
+    storagerouter_list = []
+    if not host:
+        storagerouter_list = [System.get_my_storagerouter()]
+    else:
+        if re.match(Toolbox.regex_ip, host):
+            sr = StorageRouterList.get_by_ip(host)
+            storagerouter_list = [sr]
+        if host == 'all':
+            storagerouter_list = sorted(StorageRouterList.get_storagerouters(), key=lambda k: k.name)
+        else:
+            print 'Invalid argument given. `Host` should be `all|<IP>`'
+    return storagerouter_list
+
